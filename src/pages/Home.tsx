52d3977550903a14c93f17aee4ce85d6ddb16f1d
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-08 15:31:14 -0300

    refac: add base header and base loading

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,35 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Header } from "../components/Header";
+import { Loading } from "../components/Loading";
 import { ITitleOverview } from "../interfaces/interface";
 import { getOverview } from "../services/api";
 
-export const Home = () => {
+export function Home() {
   const [apiData, setApiData] = useState<ITitleOverview>();
 
+  const [loadingData, setLoadingData] = useState<boolean>(false);
+
   async function fetchOverview() {
+    setLoadingData(true);
     const data = await getOverview("tt0944947");
     if (data) {
       setApiData(data);
-      console.log("tem data");
-
-      console.log(data);
     }
+    setLoadingData(false);
   }
-
   useEffect(() => {
     fetchOverview();
   }, []);
 
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center'>
+    <div className='h-full w-full flex flex-col'>
+      <Header />
+      <Loading show={loadingData} />
       <p className='text-7xl'>Home</p>
       <Link to='/details'>Detalhes</Link>
       <Link to='/nothing'>404</Link>
       <p>{apiData ? `Data: ${apiData.title.title}` : "Sem data"}</p>
     </div>
   );
-};
+}
