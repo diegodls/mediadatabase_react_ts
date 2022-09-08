ae5954a47554767c3cbebd02fc8ab020e3c456b3
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-08 15:29:22 -0300

    refac: add Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Details } from "./pages/Details";
 import { Home } from "./pages/Home";
+import { Details } from "./pages/Details";
+import { Search } from "./pages/Search";
 import { NotFound } from "./pages/NotFound";
 
-function App() {
+export function App() {
   return (
     <BrowserRouter>
       <div className='h-screen w-screen'>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/details' element={<Details />} />
+          <Route path='/search' element={<Search />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
     </BrowserRouter>
   );
 }
-
-export default App;
