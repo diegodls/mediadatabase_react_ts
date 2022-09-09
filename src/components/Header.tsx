fc1ca87bdfdd53e34e8f8dc3cd053a1c638a2f6a
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-09 15:03:33 -0300

    refac: creating search input

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,36 @@
-import { MagnifyingGlass } from "phosphor-react";
+import { Link } from "react-router-dom";
 import { SearchInput } from "./SearchInput";
 
 export function Header() {
   return (
-    <div className='w-full h-9 bg-green-500 flex flex-row items-center justify-around px-3'>
-      <div className='w-28 h-full flex items-center justify-center bg-blue-500 lg:bg-amber-400'>
+    <header className='w-full h-10 flex flex-row items-center justify-between'>
+      <div className='w-28 h-full px-2 flex items-center justify-center'>
         <p>LOGO</p>
       </div>
-      <div className='w-full h-9 flex flex-row items-center justify-center gap-5 bg-red-600'>
-        <p>HOME</p>
-        <p>SERIES</p>
-        <p>MOVIES</p>
+      <div className='h-full pr-20 md:pl-28 md:pr-0 sm:pr-20 flex flex-row items-center justify-center gap-5'>
+        <nav className='h-full flex items-center justify-center'>
+          <Link to='/' className='h-full flex items-center justify-center px-2'>
+            HOME
+          </Link>
+        </nav>
+        <nav className='h-full flex items-center justify-center'>
+          <Link
+            to='/movies'
+            className='h-full flex items-center justify-center px-2'
+          >
+            MOVIES
+          </Link>
+        </nav>
+        <nav className='h-full flex items-center justify-center'>
+          <Link
+            to='/series'
+            className='h-full flex items-center justify-center px-2'
+          >
+            SERIES
+          </Link>
+        </nav>
       </div>
       <SearchInput />
-    </div>
+    </header>
   );
 }
