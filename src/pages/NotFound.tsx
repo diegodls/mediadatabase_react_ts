25f80a99d8c0ed584ea0d6e46044b761a7590502
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-09 15:06:32 -0300

    refac: add back link

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,11 @@ export const NotFound = () => {
   return (
     <div className='h-full w-full flex items-center justify-center'>
       <p>Not Found</p>
-      <p>Voltar</p>
+      <nav className='h-full flex items-center justify-center '>
+        <Link to='/' className='h-full flex items-center justify-center '>
+          VOLTAR
+        </Link>
+      </nav>
     </div>
   );
 };
