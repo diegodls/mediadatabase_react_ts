d2b5e28c4ada111bc283d4a71b94db3122f6badd
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-13 16:00:27 -0300

    refac: create custom navlink

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,35 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { SearchInput } from "./SearchInput";
 
 export function Header() {
+  function CustomNavLink(name: string, path: string) {
+    return (
+      <>
+        <nav className='h-full flex items-center justify-center'>
+          <NavLink
+            to={path}
+            className={({ isActive }) => {
+              return `h-full flex items-center justify-center px-2 hover:bg-green-500 ${
+                isActive ? "border-b-2 border-red-600 bg-slate-400/25" : ""
+              }`;
+            }}
+          >
+            {name}
+          </NavLink>
+        </nav>
+      </>
+    );
+  }
+
   return (
-    <header className='w-full h-10 flex flex-row items-center justify-between'>
-      <div className='w-28 h-full px-2 flex items-center justify-center'>
+    <header className='w-full h-10 flex flex-row items-center justify-between fixed'>
+      <div className='w-28 min-w-max h-full px-2 flex items-center justify-center'>
         <p>LOGO</p>
       </div>
-      <div className='h-full pr-20 md:pl-28 md:pr-0 sm:pr-20 flex flex-row items-center justify-center gap-5'>
-        <nav className='h-full flex items-center justify-center'>
-          <Link to='/' className='h-full flex items-center justify-center px-2'>
-            HOME
-          </Link>
-        </nav>
-        <nav className='h-full flex items-center justify-center'>
-          <Link
-            to='/movies'
-            className='h-full flex items-center justify-center px-2'
-          >
-            MOVIES
-          </Link>
-        </nav>
-        <nav className='h-full flex items-center justify-center'>
-          <Link
-            to='/series'
-            className='h-full flex items-center justify-center px-2'
-          >
-            SERIES
-          </Link>
-        </nav>
+      <div className='h-full mr-20 md:ml-28 md:mr-0 sm:mr-20 flex flex-row items-center justify-center gap-5'>
+        {CustomNavLink("HOME", "/")}
+        {CustomNavLink("MOVIES", "/movies")}
+        {CustomNavLink("SERIES", "/series")}
       </div>
       <SearchInput />
     </header>
