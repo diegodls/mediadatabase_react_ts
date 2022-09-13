1737a4b197eeb10bbc78c66f4cf18d89cf415f71
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-13 16:02:46 -0300

    refac: moving code to custom component/hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Loading } from "../components/Loading";
+import { TrendingMovieAndSeries } from "../components/TrendingMovieAndSeries";
 import { IMovieDetails } from "../interfaces/IMovieDetails";
-import { getMoviesDetails, getTrending } from "../services/api";
+import { getMoviesDetails } from "../services/api";
 
 export function Home() {
   const [apiMovieDBData, setMovieDBApiData] = useState<IMovieDetails>();
@@ -11,29 +12,27 @@ export function Home() {
   const [loadingData, setLoadingData] = useState<boolean>(false);
 
   async function fetchOverview() {
-    setLoadingData(true);
     const movie = await getMoviesDetails(76341);
-
-    const trending = getTrending("movie", "week");
 
     if (movie) {
       setMovieDBApiData(movie);
     }
-
-    setLoadingData(false);
   }
   useEffect(() => {
     fetchOverview();
   }, []);
 
+  console.log("Home Rendering");
+
   return (
     <div className='h-full w-full flex flex-col items-center'>
       <Header />
-      <Loading show={loadingData} />
+      <TrendingMovieAndSeries />
       <p className='text-7xl'>Home</p>
       <Link to='/details'>Detalhes</Link>
       <Link to='/nothing'>404</Link>
       <p>{apiMovieDBData ? `Data: ${apiMovieDBData.title}` : "Sem movie"}</p>
+      <Loading show={loadingData} />
     </div>
   );
 }
