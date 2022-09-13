bcdd2276445f900bf5684072120e2d91773738cf
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-13 16:01:09 -0300

    refac: remove unused property

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 export function SearchInput() {
   return (
-    <div className='sm:w-8 md:w-auto h-full flex items-center justify-center'>
+    <div className='md:w-auto h-full flex items-center justify-center'>
       <Link
         to='/search'
         title='Search'
-        className='px-1 h-full flex items-center justify-end md:hidden'
+        className='px-1 h-full flex items-center md:hidden'
       >
         <MagnifyingGlass size={20} weight='bold' />
       </Link>
 
-      <div className='border-none rounded bg-slate-400/25 p-1 px-2 md:flex flex-row items-center justify-center hidden animated'>
+      <div className='border-none rounded bg-slate-400/25 p-1 px-2 md:flex flex-row items-center justify-center hidden'>
         <input
           type='text'
           placeholder='Search'
