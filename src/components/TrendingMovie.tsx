b988b70561b96390466084c7627959190f20f384
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-15 16:33:23 -0300

    refac: styling slide

--- src/components/TrendingMovie.tsx
+++ src/components/TrendingMovie.tsx
@@ -2,7 +2,7 @@ import { ArrowCircleLeft, ArrowCircleRight } from "phosphor-react";
 import { useEffect, useRef, useState } from "react";
 import { useTrendingMovies } from "../hooks/useTrendingMovies";
 import { ITrendingResult } from "../interfaces/ITrending";
-import { API_URL_IMAGE_500 } from "../utils/constants";
+import { API_BASEURL_IMAGE_1280 } from "../utils/constants";
 
 let count = 0;
 
@@ -13,14 +13,15 @@ export function TrendingMovie() {
 
   const [currentIndex, setCurrentIndex] = useState<number>(0);
 
-  let slideRef = useRef<HTMLDivElement>(null);
+  let slideMouseOverRef = useRef<HTMLDivElement>(null);
+  let slideAnimationRef = useRef<HTMLDivElement>(null);
   let slideInterval: ReturnType<typeof setInterval>;
 
   function handleNext() {
     if (slideArray.length > 0) {
       count = (count + 1) % slideArray.length;
       setCurrentIndex(count);
-      slideRef.current?.classList.add("animate-fadeIn");
+      slideAnimationRef.current?.classList.add("animate-fadeIn");
     }
   }
 
@@ -28,7 +29,7 @@ export function TrendingMovie() {
     if (slideArray.length > 0) {
       count = (currentIndex + slideArray.length - 1) % slideArray.length;
       setCurrentIndex(count);
-      slideRef.current?.classList.add("animate-fadeIn");
+      slideAnimationRef.current?.classList.add("animate-fadeIn");
     }
   }
 
@@ -41,7 +42,7 @@ export function TrendingMovie() {
   }
 
   function removeAnimation() {
-    slideRef.current?.classList.remove("animate-fadeIn");
+    slideAnimationRef.current?.classList.remove("animate-fadeIn");
   }
 
   function pauseInterval() {
@@ -49,9 +50,12 @@ export function TrendingMovie() {
   }
 
   useEffect(() => {
-    slideRef.current?.addEventListener("animationend", removeAnimation);
-    slideRef.current?.addEventListener("mouseleave", startSlide);
-    slideRef.current?.addEventListener("mouseenter", pauseInterval);
+    slideAnimationRef.current?.addEventListener(
+      "animationend",
+      removeAnimation
+    );
+    slideMouseOverRef.current?.addEventListener("mouseleave", startSlide);
+    slideMouseOverRef.current?.addEventListener("mouseenter", pauseInterval);
     startSlide();
 
     return () => {
@@ -60,32 +64,51 @@ export function TrendingMovie() {
   }, [slideArray]);
 
   return (
-    <div>
+    <>
       {slideArray && slideArray?.length > 0 ? (
-        <>
-          <div ref={slideRef} className='relative z-0'>
-            <div className='aspect-video'>
-              <img
-                src={API_URL_IMAGE_500 + slideArray[currentIndex].backdrop_path}
-                alt=''
-              />
-            </div>
-            <div className='absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex items-center justify-between'>
-              <button onClick={handlePrev}>
-                <ArrowCircleLeft size={32} weight='bold' />
-              </button>
-              <button onClick={handleNext}>
-                <ArrowCircleRight size={32} weight='bold' />
-              </button>
-            </div>
-            <p>
-              {slideArray.length} - {count}
-            </p>
+        <div
+          ref={slideMouseOverRef}
+          className='w-full max-h-[28rem] relative z-0'
+        >
+          <div
+            ref={slideAnimationRef}
+            className='w-full max-h-[28rem] aspect-video bg-green-500 overflow-hidden'
+          >
+            <div className='w-full top-0 h-10 bg-gradient-to-b from-bg-custom-background absolute z-10' />
+            <div className='w-full bottom-0 h-12 bg-gradient-to-t from-bg-custom-background absolute z-10' />
+            <img
+              className='w-full h-auto transform lg:-translate-y-[8rem]'
+              src={
+                API_BASEURL_IMAGE_1280 + slideArray[currentIndex].backdrop_path
+              }
+              alt={slideArray[currentIndex].title}
+              title={slideArray[currentIndex].title}
+            />
           </div>
-        </>
+          <div className='absolute w-full top-1/2 transform -translate-y-1/2 px-3 flex items-center justify-between'>
+            <button onClick={handlePrev}>
+              <ArrowCircleLeft size={32} weight='bold' />
+            </button>
+            <button onClick={handleNext}>
+              <ArrowCircleRight size={32} weight='bold' />
+            </button>
+          </div>
+          <div className='flex flex-row justify-between'>
+            {trendingMovies?.map((value: ITrendingResult, index: number) => {
+              return (
+                <div
+                  key={index}
+                  className={`w-4 h-4 rounded-full bg-red-500 ${
+                    currentIndex === index ? "bg-green-500" : ""
+                  }`}
+                ></div>
+              );
+            })}
+          </div>
+        </div>
       ) : (
         "Carregando..."
       )}
-    </div>
+    </>
   );
 }
