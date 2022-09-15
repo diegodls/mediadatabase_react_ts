947c0e8630edfba59ff9863f978bba4f894554e8
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-15 16:31:05 -0300

    refac: add custom color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,14 +3,17 @@ module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
     extend: {
+      colors: {
+        "bg-custom-background": "#131313",
+      },
       keyframes: {
         fadeIn: {
-          "0%": { opacity: 0},
-          "100%": { opacity: 100},
+          "0%": { opacity: 0 },
+          "100%": { opacity: 100 },
         },
         fadeOut: {
-          "0%": { opacity: 100},
-          "100%": { opacity: 0},
+          "0%": { opacity: 100 },
+          "100%": { opacity: 0 },
         },
       },
       animation: {
