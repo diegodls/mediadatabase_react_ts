6f6485b318cdd4b50e3c06e81c036de80e71d6e2
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-09-15 16:33:43 -0300

    refac: add image size (1280px)

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,3 @@
-export const API_URL_IMAGE_500: string = "https://image.tmdb.org/t/p/w500";
+export const API_BASEURL_IMAGE_500: string = "https://image.tmdb.org/t/p/w500";
+export const API_BASEURL_IMAGE_1280: string =
+  "https://image.tmdb.org/t/p/w1280";
