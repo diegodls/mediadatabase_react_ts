9ea79507209e0662f34eab7868b3ba82ab290b75
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-10-07 11:37:38 -0300

    refac: making navbar absolute to always center

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,18 +18,21 @@ export function Header() {
   }
 
   return (
-    <header className='w-full h-10 flex flex-row items-center justify-between fixed z-50 px-2'>
-      <div className='w-28 min-w-max h-full px-2 flex items-center justify-center'>
-        <p>LOGO</p>
+    <header className='w-full h-10 flex flex-row items-center justify-center absolute z-50 overflow-hidden '>
+      <div className='w-full h-full px-2 flex items-center justify-between'>
+        <span className='max-w-[4em] sm:max-w-[7em] h-full flex items-center justify-center overflow-hidden'>
+          LOGO
+        </span>
+        <SearchInput />
       </div>
-      <div className='h-full mr-20 md:ml-28 md:mr-0 sm:mr-20 flex flex-row items-center justify-center gap-5'>
+
+      <div className='h-full w-full flex flex-row items-center justify-center gap-5 absolute'>
         <nav className='h-full flex items-center justify-center'>
           {CustomNavLink("HOME", "/")}
           {CustomNavLink("MOVIES", "/movies")}
           {CustomNavLink("SERIES", "/series")}
         </nav>
       </div>
-      <SearchInput />
     </header>
   );
 }
