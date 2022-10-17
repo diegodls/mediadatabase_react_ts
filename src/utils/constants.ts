7a8da10b190d98c232f87118fb254697bae30a50
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-10-17 15:51:24 -0300

    refac: moving movie genre list

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,87 @@
+export const API_BASEURL_IMAGE_200: string = "https://image.tmdb.org/t/p/w200";
 export const API_BASEURL_IMAGE_400: string = "https://image.tmdb.org/t/p/w400";
 export const API_BASEURL_IMAGE_500: string = "https://image.tmdb.org/t/p/w500";
 export const API_BASEURL_IMAGE_780: string = "https://image.tmdb.org/t/p/w780";
 export const API_BASEURL_IMAGE_1280: string =
   "https://image.tmdb.org/t/p/w1280";
+
+export const MovieTypeList = {
+  genres: [
+    {
+      id: 28,
+      name: "Ação",
+    },
+    {
+      id: 12,
+      name: "Aventura",
+    },
+    {
+      id: 16,
+      name: "Animação",
+    },
+    {
+      id: 35,
+      name: "Comédia",
+    },
+    {
+      id: 80,
+      name: "Crime",
+    },
+    {
+      id: 99,
+      name: "Documentário",
+    },
+    {
+      id: 18,
+      name: "Drama",
+    },
+    {
+      id: 10751,
+      name: "Família",
+    },
+    {
+      id: 14,
+      name: "Fantasia",
+    },
+    {
+      id: 36,
+      name: "História",
+    },
+    {
+      id: 27,
+      name: "Terror",
+    },
+    {
+      id: 10402,
+      name: "Música",
+    },
+    {
+      id: 9648,
+      name: "Mistério",
+    },
+    {
+      id: 10749,
+      name: "Romance",
+    },
+    {
+      id: 878,
+      name: "Ficção científica",
+    },
+    {
+      id: 10770,
+      name: "Cinema TV",
+    },
+    {
+      id: 53,
+      name: "Thriller",
+    },
+    {
+      id: 10752,
+      name: "Guerra",
+    },
+    {
+      id: 37,
+      name: "Faroeste",
+    },
+  ],
+};
