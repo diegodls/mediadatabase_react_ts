6ed33e9562bfead62c945163d6b2370bae56421f
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-10-18 11:07:26 -0300

    refac: creating skeleton (still working on it)

--- src/components/PopularMovies.tsx
+++ src/components/PopularMovies.tsx
@@ -4,29 +4,45 @@ import { API_BASEURL_IMAGE_200 } from "../utils/constants";
 
 export function PopularMovies() {
   const { popularMovies } = usePopularMovies();
+
   return (
     <>
-      {popularMovies && popularMovies?.length && (
-        <>
-          <div className='w-full h-[266px] bg-red-500 '>
-            <div className='w-full h-full flex flex-row items-center gap-2 bg-blue-500 relative'>
-              {popularMovies.map((movie: IPopularMoviesResults) => (
-                <div className='group min-w-[178px] h-full flex flex-col rounded-md relative bg-green-500'>
-                  <div className='w-full bottom-0 group-hover:bottom-0 absolute z-20 animate bg-fuchsia-500'>
-                    <p className='absolute z-20'>{movie.title}</p>
-
-                    <div className='w-full h-12 bg-gradient-to-t from-customColors-background absolute z-10' />
-                  </div>
-
-                  <img
-                    src={`${API_BASEURL_IMAGE_200 + movie.poster_path}`}
-                    className='h-full'
-                  />
+      {popularMovies && popularMovies?.length > 0 ? (
+        <div className='w-full h-32 md:h-64 flex flex-row  gap-2 overflow-hidden'>
+          {Array(3)
+            .fill(null)
+            .map((_, index, array) => (
+              <div
+                className={`w-48 h-full rounded-md 
+                bg-slate-500/${(array.length - index) * 5} overflow-hidden`}
+              >
+                <div className=' h-64 animate-diagonalMove'>
+                  <div className='w-12 h-[500px] bg-gradient-to-r from-transparent via-stone-800/90 to-transparent transform rotate-45 translate-y-[-250px]' />
                 </div>
-              ))}
-            </div>
+              </div>
+            ))}
+        </div>
+      ) : (
+        <div className='w-full h-32 md:h-64 overflow-hidden'>
+          <div className='w-full h-full flex flex-row items-center gap-2 relative overflow-hidden'>
+            {popularMovies?.map((movie: IPopularMoviesResults) => (
+              <div className='group min-w-fit h-full flex flex-col rounded-md relative overflow-hidden'>
+                <div className='w-full h-12 flex items-center bottom-[-3rem] group-hover:bottom-0 absolute z-20 group-hover:transition-all'>
+                  <p className='p-2 font-bold absolute z-20 line-clamp-2'>
+                    {movie.title}
+                  </p>
+
+                  <div className='w-full h-12 bg-gradient-to-t from-customColors-background absolute z-10' />
+                </div>
+
+                <img
+                  src={`${API_BASEURL_IMAGE_200 + movie.poster_path}`}
+                  className='h-full'
+                />
+              </div>
+            ))}
           </div>
-        </>
+        </div>
       )}
     </>
   );
