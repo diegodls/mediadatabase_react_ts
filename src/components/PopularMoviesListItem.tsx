0e5eef72e5096d0c879d07a9a682e3a2df0b9a2f
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-07 15:37:06 -0300

    refac: add scale animation

--- src/components/PopularMoviesListItem.tsx
+++ src/components/PopularMoviesListItem.tsx
@@ -7,9 +7,9 @@ interface PopularMoviesListItemProps {
 
 export function PopularMoviesListItem({ movie }: PopularMoviesListItemProps) {
   return (
-    <li className='group min-w-fit h-full flex flex-col rounded-md relative overflow-hidden'>
-      <div className='w-full h-12 flex items-center absolute z-20 bottom-0 translate-y-12 group-hover:translate-y-0 transition-all ease-out opacity-0 group-hover:opacity-100'>
-        <p className='font-bold pl-2 absolute z-20 line-clamp-1 overflow-hidden'>
+    <li className='group min-w-fit h-full flex flex-col rounded-md cursor-pointer relative overflow-hidden transform hover:scale-110 transition-all'>
+      <div className='w-full h-12 flex items-center absolute z-20 bottom-0 translate-y-12 group-hover:translate-y-0 transition-all ease-in opacity-0 group-hover:opacity-100'>
+        <p className='font-bold pl-2 absolute z-20 line-clamp-1 overflow-hidden scale-90'>
           {movie.title}
         </p>
 
