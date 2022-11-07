440b88f9292ae79426e16c7ce4d820a828b08a9e
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-07 11:53:27 -0300

    refac: move opacity function to utils

--- src/components/PopularMoviesListItemSkeleton.tsx
+++ src/components/PopularMoviesListItemSkeleton.tsx
@@ -1,3 +1,5 @@
+import { calcOpacity } from "../utils/calcOpacity";
+
 interface PopularMoviesListItemSkeletonProps {
   itemIndex: number;
   array: null[];
@@ -7,31 +9,14 @@ export function PopularMoviesListItemSkeleton({
   itemIndex,
   array,
 }: PopularMoviesListItemSkeletonProps) {
-  function calcOpacity(index: number, length: number) {
-    const minOpacity: number = 10;
-    const maxOpacity: number = 50;
-
-    if (minOpacity >= maxOpacity) {
-      return maxOpacity;
-    }
-
-    const opacityInterval = (maxOpacity - minOpacity) / (length - 1);
-    const finalOpacity =
-      (minOpacity + opacityInterval * (length - 1 - index)) / 100; //remover o "length -1" para mudar a ordem
-    return finalOpacity.toFixed(1);
-  }
-
+  const op = calcOpacity(itemIndex, array.length);
   return (
     <li
-      className={`w-48 h-full rounded-md 
-                bg-slate-500 opacity-[${calcOpacity(
-                  itemIndex,
-                  array.length
-                )}] overflow-hidden`}
+      className={`opacity-${op} w-36 h-full rounded-md bg-gray-600 overflow-hidden `}
     >
-      <h1>{calcOpacity(itemIndex, array.length)}</h1>
-      <div className=' h-64 animate-diagonalMove'>
-        <div className='w-12 h-[500px] bg-gradient-to-r from-transparent via-stone-800/90 to-transparent transform rotate-45 translate-y-[-250px]' />
+      <p className='absolute'>{calcOpacity(itemIndex, array.length)}</p>
+      <div className=' animate-diagonalMove'>
+        <div className='w-52 h-96 bg-gradient-to-r from-transparent via-slate-50/30 to-transparent transform rotate-45 translate-x-[-50%] translate-y-[-50%]' />
       </div>
     </li>
   );
