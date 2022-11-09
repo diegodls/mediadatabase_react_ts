7bb5aba1473c9c980f30392291dfa05cc342dc9d
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-09 11:04:34 -0300

    refac: add min width & remove comments

--- src/components/PopularMoviesListItemSkeleton.tsx
+++ src/components/PopularMoviesListItemSkeleton.tsx
@@ -13,12 +13,10 @@ export function PopularMoviesListItemSkeleton({
 
   return (
     <li
-      className={`w-36 h-full rounded-md bg-gray-600 overflow-hidden`}
+      className={`md:w-36 md:min-w-[9rem] h-full rounded-md bg-gray-600 overflow-hidden`}
       style={{ opacity: dynamicOpacity }}
     >
-      <p>{calcDynamicOpacity(itemIndex, array.length)}</p>
-      <p>{dynamicOpacity}</p>
-      <div className=' animate-diagonalMove'>
+      <div className='animate-diagonalMove'>
         <div className='w-52 h-96 bg-gradient-to-r from-transparent via-slate-50/30 to-transparent transform rotate-45 translate-x-[-50%] translate-y-[-50%]' />
       </div>
     </li>
