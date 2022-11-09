736b10e78cdec07a7616c9d996c42d67259ad137
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-09 14:17:33 -0300

    refac: add scroll function

--- src/components/PopularMoviesList.tsx
+++ src/components/PopularMoviesList.tsx
@@ -1,4 +1,5 @@
 import { CaretLeft, CaretRight } from "phosphor-react";
+import { useState } from "react";
 import { usePopularMovies } from "../hooks/usePopularMovies";
 import { IPopularMoviesResults } from "../interfaces/IPopularMovies";
 import { PopularMoviesListItem } from "./PopularMoviesListItem";
@@ -6,20 +7,72 @@ import { PopularMoviesListItemSkeleton } from "./PopularMoviesListItemSkeleton";
 
 export function PopularMoviesList() {
   const { popularMovies } = usePopularMovies();
+
+  const [scrollX, setScrollX] = useState<number>(36);
+
+  const itemWidth = 150; //in pixels
+  const scrollButtonWidth = 36; //in pixels
+  const itemGap = 8; //in pixels
+  const itemsGapAmount = popularMovies ? (popularMovies?.length - 1) * 8 : 0;
+
+  function handleScrollLeft() {
+    let x = scrollX + Math.round(window.innerWidth / 2);
+
+    if (x > scrollButtonWidth || x > scrollButtonWidth - itemWidth) {
+      x = scrollButtonWidth;
+    }
+
+    setScrollX(x);
+  }
+
+  function handleScrollRight() {
+    let listWidth = popularMovies
+      ? popularMovies?.length * itemWidth + itemsGapAmount
+      : 0;
+
+    let listCompleteWidth =
+      (listWidth - window.innerWidth + scrollButtonWidth + itemGap) * -1;
+
+    let x = scrollX - Math.round(window.innerWidth / 2);
+
+    if (x < scrollX - itemWidth - itemGap) {
+      (listWidth - window.innerWidth + scrollButtonWidth + itemGap) * -1;
+    }
+
+    if (window.innerWidth - listWidth > x) {
+      x = window.innerWidth - listWidth - scrollButtonWidth - itemGap;
+    }
+
+    if (x - listCompleteWidth < itemWidth + itemGap) {
+      x = listCompleteWidth;
+    }
+
+    setScrollX(x);
+  }
+
   //popularMovies && popularMovies?.length > 0
+
   return (
     <>
+      {scrollX}
       <div className='group/edit w-full h-44 md:h-56 flex items-center relative top-[-10px]'>
-        <span className='h-full flex items-center bg-black/50 hover:bg-black/80 rounded-r-sm overflow-hidden absolute left-0 z-20 cursor-pointer opacity-100 group-hover/edit:opacity-100 transition-all'>
+        <span
+          onClick={handleScrollLeft}
+          className='w-9 h-full flex items-center justify-center bg-black/50 hover:bg-black/80 rounded-r-sm overflow-hidden absolute left-0 z-20 cursor-pointer opacity-100 group-hover/edit:opacity-100 transition-all select-none'
+        >
           <CaretLeft size={32} color='#ffffff' weight='fill' />
         </span>
-        <span className='h-full flex items-center bg-black/50 hover:bg-black/80 rounded-l-sm overflow-hidden absolute right-0 z-20 cursor-pointer opacity-100 group-hover/edit:opacity-100 transition-all'>
+        <span
+          onClick={handleScrollRight}
+          className='w-9 h-full flex items-center justify-center bg-black/50 hover:bg-black/80 rounded-l-sm overflow-hidden absolute right-0 z-20 cursor-pointer opacity-100 group-hover/edit:opacity-100 transition-all select-none'
+        >
           <CaretRight size={32} color='#ffffff' weight='fill' />
         </span>
         {popularMovies && popularMovies?.length > 0 ? (
           <ul
-            className='w-full h-full flex flex-row items-center gap-2 ml-9 relative transform'
+            className='w-full h-full flex flex-row items-center gap-2 relative transition-all'
             role='list'
+            style={{ marginLeft: scrollX }}
           >
             {popularMovies?.map((movie: IPopularMoviesResults) => (
               <PopularMoviesListItem movie={movie} key={movie.id} />
