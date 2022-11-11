91ddc2936ff50f6921b2c8adfbb5df2bbaeeb9c2
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-11 13:46:20 -0300

    refac: rebuilding generic component

--- src/components/PopularMoviesList.tsx
+++ src/components/PopularMoviesList.tsx
@@ -1,13 +1,21 @@
 import { CaretLeft, CaretRight } from "phosphor-react";
 import { useRef } from "react";
-import { usePopularMovies } from "../hooks/usePopularMovies";
-import { IPopularMoviesResults } from "../interfaces/IPopularMovies";
 import { PopularMoviesListItem } from "./PopularMoviesListItem";
 import { PopularMoviesListItemSkeleton } from "./PopularMoviesListItemSkeleton";
 
-export function PopularMoviesList() {
-  const { popularMovies } = usePopularMovies();
+interface IListRowProps<T> {
+  rowTitle?: string;
+  type?: string;
+  data?: T[];
+}
 
+export function PopularMoviesList<T>({
+  rowTitle,
+  type,
+  data,
+}: IListRowProps<
+  T & { id: number; title?: string; name?: string; poster_path?: string }
+>) {
   const listRef = useRef<HTMLUListElement>(null);
 
   function handleScrollLeft(
@@ -34,8 +42,14 @@ export function PopularMoviesList() {
 
   //popularMovies && popularMovies?.length > 0
 
+  console.log("----------------DATA");
+  console.log(data && data[0]);
+
   return (
     <>
+      <p>rowTitle: {rowTitle}</p>
+      <p>type: {type}</p>
+      {data ? <p>{data[data.length - 1].name}</p> : null}
       <div className='group/edit w-full h-48 md:h-64 flex items-center relative top-[-10px]'>
         <button
           aria-label='Scroll para esquerda'
@@ -53,14 +67,22 @@ export function PopularMoviesList() {
         >
           <CaretRight size={32} color='#ffffff' weight='fill' />
         </button>
-        {popularMovies && popularMovies?.length > 0 ? (
+        {data && data?.length > 0 ? (
           <ul
             className='w-full h-full px-9 flex flex-row items-center relative transition-all overflow-x-scroll scroll-smooth scrollbar-hide'
             ref={listRef}
             role='list'
           >
-            {popularMovies?.map((movie: IPopularMoviesResults) => (
-              <PopularMoviesListItem movie={movie} key={movie.id} />
+            {data?.map((item) => (
+              <PopularMoviesListItem
+                title={item.title || item.name}
+                poster_path={
+                  item.poster_path
+                    ? item.poster_path
+                    : "https://cdn.w600.comps.canstockphoto.com.br/projetos-poster-glitched-tipogr%C3%A1fico-vetor-clip-arte_csp40896763.jpg"
+                }
+                key={item.id}
+              />
             ))}
           </ul>
         ) : (
