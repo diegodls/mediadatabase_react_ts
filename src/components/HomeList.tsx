32a1b91fc90e27abe1749447837d5f4a219fdb92
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-16 11:54:27 -0300

    refac: change scroll buttons place

--- src/components/HomeList.tsx
+++ src/components/HomeList.tsx
@@ -1,5 +1,6 @@
 import { CaretLeft, CaretRight } from "phosphor-react";
 import { useRef, useState } from "react";
+import { usePopularMovies } from "../hooks/usePopularMovies";
 import { HomeListItem } from "./HomeListItem";
 import { HomeListItemSkeleton } from "./HomeListItemSkeleton";
 
@@ -16,6 +17,7 @@ export function HomeList<T>({
 }: IListRowProps<
   T & { id: number; title?: string; name?: string; poster_path?: string }
 >) {
+  const { popularMovies } = usePopularMovies();
   const listRef = useRef<HTMLUListElement>(null);
 
   const [isMouseOverList, setIsMouseOverList] = useState<boolean>(false);
@@ -36,16 +38,14 @@ export function HomeList<T>({
   ) {
     e.preventDefault();
     if (listRef.current) {
-      listRef.current.scrollLeft += listRef.current
-        ? listRef.current?.offsetWidth
-        : 0;
+      listRef.current.scrollLeft += listRef.current?.offsetWidth;
     }
   }
 
   //popularMovies && popularMovies?.length > 0
 
   return (
-    <div className={`w-full flex flex-col relative mt-2`}>
+    <div className='w-full flex flex-col relative mt-2'>
       <strong className='px-5 text-lg'>{rowTitle}</strong>
       <div
         onMouseEnter={() => {
@@ -56,47 +56,49 @@ export function HomeList<T>({
         }}
         className='w-full h-48 md:h-64 flex flex-col relative'
       >
-        <button
-          aria-label='Scroll para esquerda'
-          title='Scroll para esquerda'
-          onClick={handleScrollLeft}
-          className={`w-9 h-full flex items-center justify-center bg-black/50 hover:bg-black/80 rounded-r-sm overflow-hidden absolute left-0 z-20 cursor-pointer transition-all select-none ${
-            isMouseOverList ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <CaretLeft size={32} color='#ffffff' weight='fill' />
-        </button>
-        <button
-          aria-label='Scroll para direita'
-          title='Scroll para direita'
-          onClick={handleScrollRight}
-          className={`w-9 h-full flex items-center justify-center bg-black/50 hover:bg-black/80 rounded-l-sm overflow-hidden absolute right-0 z-20 cursor-pointer transition-all select-none  ${
-            isMouseOverList ? "opacity-100" : "opacity-0"
-          }`}
-        >
-          <CaretRight size={32} color='#ffffff' weight='fill' />
-        </button>
-        {data && data?.length > 0 ? (
-          <ul
-            className='w-full px-10 flex flex-row items-center relative transition-all overflow-x-scroll scroll-smooth scrollbar-hide'
-            ref={listRef}
-            role='list'
-          >
-            {data?.map((item) => (
-              <HomeListItem
-                title={item.title || item.name}
-                poster_path={
-                  item.poster_path
-                    ? item.poster_path
-                    : "https://cdn.w600.comps.canstockphoto.com.br/projetos-poster-glitched-tipogr%C3%A1fico-vetor-clip-arte_csp40896763.jpg"
-                }
-                key={item.id}
-              />
-            ))}
-          </ul>
+        {popularMovies && popularMovies?.length > 0 ? (
+          <div className='w-full h-full relative'>
+            <button
+              aria-label='Scroll para esquerda'
+              title='Scroll para esquerda'
+              onClick={handleScrollLeft}
+              className={`w-9 h-full flex items-center justify-center bg-black/50 hover:bg-black/80 rounded-r-sm overflow-hidden absolute left-0 z-20 cursor-pointer transition-all select-none ${
+                isMouseOverList ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <CaretLeft size={32} color='#ffffff' weight='fill' />
+            </button>
+            <button
+              aria-label='Scroll para direita'
+              title='Scroll para direita'
+              onClick={handleScrollRight}
+              className={`w-9 h-full flex items-center justify-center bg-black/50 hover:bg-black/80 rounded-l-sm overflow-hidden absolute right-0 z-20 cursor-pointer transition-all select-none  ${
+                isMouseOverList ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <CaretRight size={32} color='#ffffff' weight='fill' />
+            </button>
+            <ul
+              className='w-full h-full px-10 flex flex-row items-center relative transition-all overflow-x-scroll scroll-smooth scrollbar-hide'
+              ref={listRef}
+              role='list'
+            >
+              {data?.map((item) => (
+                <HomeListItem
+                  title={item.title || item.name}
+                  poster_path={
+                    item.poster_path
+                      ? item.poster_path
+                      : "https://cdn.w600.comps.canstockphoto.com.br/projetos-poster-glitched-tipogr%C3%A1fico-vetor-clip-arte_csp40896763.jpg"
+                  }
+                  key={item.id}
+                />
+              ))}
+            </ul>
+          </div>
         ) : (
           <ul
-            className='w-full h-full ml-9 flex flex-row items-center gap-2 relative overflow-hidden'
+            className='w-full h-full px-10 flex flex-row items-center relative overflow-hidden'
             role='list'
           >
             {Array(6)
