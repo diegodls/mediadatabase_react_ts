63f8e0512feac997552308fd7538f7234fd6d868
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-16 11:56:33 -0300

    refac: add function to choose one featured tvshow

--- src/hooks/usePopularTvShows.ts
+++ src/hooks/usePopularTvShows.ts
@@ -7,10 +7,13 @@ import {
 import { service } from "../services/api";
 
 export function usePopularTvShows() {
-  const [popularTvShows, setPopularTvShows] =
+  const [FeaturedPopularTvShow, setFeaturedPopularTvShow] =
+    useState<IPopularTvShowsResults>();
+
+  const [popularTvShowsWithoutFeatured, setPopularTvShowsWithoutFeatured] =
     useState<IPopularTvShowsResults[]>();
 
-  async function getPopular(): Promise<IPopularTvShowsResults[]> {
+  async function getPopularTvShows(): Promise<IPopularTvShowsResults[]> {
     const popularData: IPopularTvShowsApiReturn = await service
       .get<Promise<IPopularTvShowsApiReturn>>(`/tv/popular`)
       .then((response) => {
@@ -20,18 +23,38 @@ export function usePopularTvShows() {
     return popularData.results;
   }
 
-  async function fetchPopular() {
-    const popular = await getPopular();
+  function removeFeaturedTvShowItem<T>(arr: Array<T>, value: T): Array<T> {
+    const index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+  async function populatePopularTvShowsStates() {
+    const popular = await getPopularTvShows();
 
     if (popular) {
-      setPopularTvShows(popular);
+      let randomNumber = Math.floor(Math.random() * popular.length);
+      let popularFeaturedTvShow = popular[randomNumber];
+
+      let popularTvShowsWithoutFeatured =
+        removeFeaturedTvShowItem<IPopularTvShowsResults>(
+          popular,
+          popularFeaturedTvShow
+        );
+
+      setFeaturedPopularTvShow(popularFeaturedTvShow);
+      setPopularTvShowsWithoutFeatured(popularTvShowsWithoutFeatured);
     }
   }
+
   useEffect(() => {
-    fetchPopular();
+    populatePopularTvShowsStates();
   }, []);
 
   return {
-    popularTvShows,
+    FeaturedPopularTvShow,
+    popularTvShowsWithoutFeatured,
   };
 }
