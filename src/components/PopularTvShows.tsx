c72c1ab93ec16d21b761f7a69f33fc35c5b6e7a3
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-18 11:46:42 -0300

    refac: pre coding component

--- src/components/PopularTvShows.tsx
+++ src/components/PopularTvShows.tsx
@@ -1,3 +1,17 @@
+import { usePopularTvShows } from "../hooks/usePopularTvShows";
+import { API_BASEURL_IMAGE_1280 } from "../utils/constants";
+
 export function PopularTvShow() {
-  return <div className='w-full h-[70vh] bg-red-500'></div>;
+  const { featuredPopularTvShow } = usePopularTvShows();
+
+  return featuredPopularTvShow ? (
+    <div className='w-full relative overflow-hidden bg-red-500'>
+      <img
+        className='min-w-full flex-shrink-0 select-none bg-cover'
+        src={API_BASEURL_IMAGE_1280 + featuredPopularTvShow?.backdrop_path}
+        alt={featuredPopularTvShow?.name}
+        title={featuredPopularTvShow?.name}
+      />
+    </div>
+  ) : null;
 }
