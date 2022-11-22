b094a2fe0ebd7b081b174d726d470952c94ada57
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-22 17:04:35 -0300

    refac: changing max sizes

--- src/components/PopularTvShows.tsx
+++ src/components/PopularTvShows.tsx
@@ -5,9 +5,12 @@ export function PopularTvShow() {
   const { featuredPopularTvShow } = usePopularTvShows();
 
   return featuredPopularTvShow ? (
-    <div className='w-full relative overflow-hidden bg-red-500'>
+    <div className='w-full max-h-[80vh] flex items-center justify-center relative overflow-hidden bg-red-500'>
+      <div className='ml-16 top-0 left-0 transform translate-y-1/2 absolute bg-green-500'>
+        <h1>{featuredPopularTvShow.name}</h1>
+      </div>
       <img
-        className='min-w-full flex-shrink-0 select-none bg-cover'
+        className='w-full h-auto flex-shrink-0 select-none bg-cover'
         src={API_BASEURL_IMAGE_1280 + featuredPopularTvShow?.backdrop_path}
         alt={featuredPopularTvShow?.name}
         title={featuredPopularTvShow?.name}
