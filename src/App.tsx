1d4cd48659c8a9911db409c37027d7021fa988a0
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-11-29 11:46:11 -0300

    refac: add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import { ErrorBoundary } from "react-error-boundary";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ErrorFallback } from "./components/ErrorFallback";
 import { Details } from "./pages/Details";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
@@ -6,13 +8,15 @@ import { Search } from "./pages/Search";
 
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/details' element={<Details />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/details' element={<Details />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
