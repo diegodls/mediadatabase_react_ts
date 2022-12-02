6c32d476c3a44b9f3a6eb7018058b44c7e11d78b
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-02 12:25:11 -0300

    refac: centralized hooks imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import { Header } from "../components/Header";
 import { HomeList } from "../components/HomeList";
+import { PopularPerson } from "../components/PopularPerson";
 import { PopularTvShow } from "../components/PopularTvShows";
 import { TrendingMovie } from "../components/TrendingMovie";
+import { useGenres } from "../hooks/useGenres";
 import { usePopularMovies } from "../hooks/usePopularMovies";
+import { usePopularPersons } from "../hooks/usePopularPersons";
 import { usePopularTvShows } from "../hooks/usePopularTvShows";
 import { useTopRatedTvShows } from "../hooks/useTopRatedTvShows";
+import { useTrendingMovies } from "../hooks/useTrendingMovies";
 import { useUpcomingMovies } from "../hooks/useUpcomingMovies";
 import { IPopularMoviesResults } from "../interfaces/IPopularMovies";
 import { IPopularTvShowsResults } from "../interfaces/IPopularTvShows";
@@ -12,16 +16,23 @@ import { ITopRatedTvShowsResults } from "../interfaces/ITopRatedTvShows";
 import { IUpcomingMoviesResults } from "../interfaces/IUpcomingMovies";
 
 export function Home() {
+  const { trendingMovies } = useTrendingMovies();
   const { popularMovies } = usePopularMovies();
   const { upcomingMovies } = useUpcomingMovies();
-  const { popularTvShowsWithoutFeatured } = usePopularTvShows();
+  const { featuredPopularTvShow, popularTvShowsWithoutFeatured } =
+    usePopularTvShows();
   const { topRatedTvShows } = useTopRatedTvShows();
+  const { personList } = usePopularPersons();
+  const { TvShowsGenresList, movieGenresList } = useGenres();
 
   return (
     <div className='w-full min-w-[640px] max-w-[1366px] flex flex-col overflow-hidden'>
       <Header />
 
-      <TrendingMovie />
+      <TrendingMovie
+        trendingMovies={trendingMovies ? trendingMovies : null}
+        movieGenresList={movieGenresList ? movieGenresList : null}
+      />
 
       <div className='relative mt-[-48px]'>
         <HomeList<IPopularMoviesResults>
@@ -36,7 +47,12 @@ export function Home() {
           data={upcomingMovies}
         />
 
-        <PopularTvShow />
+        <PopularTvShow
+          featuredPopularTvShow={
+            featuredPopularTvShow ? featuredPopularTvShow : null
+          }
+          TvShowsGenresList={TvShowsGenresList ? TvShowsGenresList : null}
+        />
 
         <HomeList<IPopularTvShowsResults>
           rowTitle='Series Populares'
@@ -50,6 +66,8 @@ export function Home() {
           data={topRatedTvShows}
         />
       </div>
+
+      <PopularPerson personList={personList ? personList : null} />
     </div>
   );
 }
