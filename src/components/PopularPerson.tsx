dea1979069ffce0095f38c1715bc3cc0d5217bbf
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-05 11:32:42 -0300

    refac: add default profile picture & hover effect

--- src/components/PopularPerson.tsx
+++ src/components/PopularPerson.tsx
@@ -1,6 +1,7 @@
 import { IPerson } from "../interfaces/IPerson";
 import { API_BASEURL_IMAGE_200 } from "../utils/constants";
 
+import profile_picture_not_found from "../assets/img/profile_picture_not_found.png";
 interface PopularPersonProps {
   personList: IPerson[] | null;
 }
@@ -8,19 +9,26 @@ export function PopularPerson({ personList }: PopularPersonProps) {
   return (
     <>
       {personList && personList?.length > 0 ? (
-        <div className='w-full flex flex-row justify-around items-center bg-red-500'>
+        <div className='w-full flex flex-row justify-around items-center mt-2'>
           {personList.slice(0, 5).map((person: IPerson, _) => (
-            <div className='h-full flex flex-col items-center justify-center'>
+            <div className='h-full flex flex-col items-center justify-center p-2 rounded hover:bg-zinc-800 cursor-pointer transition-all'>
               <div
                 key={person.id}
                 className='w-32 h-32 rounded-full overflow-hidden bg-green-500'
               >
-                <img
-                  src={`${API_BASEURL_IMAGE_200}${person.profile_path}`}
-                  alt={`Foto de ${person.name}`}
-                />
+                {person.profile_path ? (
+                  <img
+                    src={`${API_BASEURL_IMAGE_200}${person.profile_path}`}
+                    alt={`Foto de ${person.name}`}
+                  />
+                ) : (
+                  <img
+                    src={`${profile_picture_not_found}`}
+                    alt={`Foto de ${person.name} não foi encontrada.`}
+                  />
+                )}
               </div>
-              <p>{person.name}</p>
+              <strong>{person.name}</strong>
             </div>
           ))}
         </div>
