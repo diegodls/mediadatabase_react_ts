1a84f7d19fb14c2140f16aee4f96c2e9b5daf33f
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-07 12:29:01 -0300

    refac: update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import { ErrorBoundary } from "react-error-boundary";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { ErrorFallback } from "./components/ErrorFallback";
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
-import { Details } from "./pages/Details";
-import { Home } from "./pages/Home";
-import { MovieOverview } from "./pages/MovieOverview";
-import { NotFound } from "./pages/NotFound";
-import { Search } from "./pages/Search";
 
 export function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/details' element={<Details />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/movie/:movieId' element={<MovieOverview />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Outlet />
+      <Footer />
     </ErrorBoundary>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,25 @@
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { App } from "./App";
 import "./main.css";
+import { Details } from "./pages/Details";
+import { Home } from "./pages/Home";
+import { MovieOverview } from "./pages/MovieOverview";
+import { NotFound } from "./pages/NotFound";
+import { Search } from "./pages/Search";
 
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/details", element: <Details /> },
+      { path: "/search", element: <Search /> },
+      { path: "/movie/:movieId", element: <MovieOverview /> },
+      { path: "*", element: <NotFound /> },
+    ],
+  },
+]);
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+  <RouterProvider router={router} />
 );
