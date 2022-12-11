7a897f3f169cf3ed19e5b754a91a4f37970b7b8f
Diego <contato.diegoluisdasilva@gmail.com>
2022-12-11 16:24:20 -0300

    refac: center content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -25,7 +25,7 @@ export function Home() {
   const { tvShowsGenresList, movieGenresList } = useGenres();
 
   return (
-    <>
+    <div className='w-full flex flex-col items-center justify-center'>
       <TrendingMovie
         trendingMovies={trendingMovies ? trendingMovies : null}
         movieGenresList={movieGenresList ? movieGenresList : null}
@@ -65,6 +65,6 @@ export function Home() {
       </div>
 
       <PopularPerson personList={personList ? personList : null} />
-    </>
+    </div>
   );
 }
