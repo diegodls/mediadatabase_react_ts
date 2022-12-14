d2d3f77baf2f4aa7a778646411c07bdf0f608423
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-14 11:51:10 -0300

    feat: add get without params

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,12 +2,21 @@ import axios from "axios";
 
 export const service = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-  timeout: 1000,
+  timeout: 3000,
   headers: {
     Authorization: `Bearer ${import.meta.env.VITE_THEMOVIEDB_TOKEN}`,
     "Content-Type": "application/json;charset=utf-8",
   },
   params: {
     language: `${import.meta.env.VITE_THEMOVIEDB_LANGUAGE}`,
+  },
+});
+
+export const serviceWithoutParams = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  timeout: 3000,
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_THEMOVIEDB_TOKEN}`,
+    "Content-Type": "application/json;charset=utf-8",
   },
 });
