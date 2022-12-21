de8595c90bf9bc5307ac1f5400bbf91d146f1a16
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-21 12:01:29 -0300

    refac: change title to component

--- src/components/SimilarMovies.tsx
+++ src/components/SimilarMovies.tsx
@@ -1,4 +1,5 @@
 import { ISimilarMoviesResult } from "../interfaces/ISimilarMovies";
+import { SectionTitle } from "./IUSharedComponents/SectionTitle";
 
 interface ISimilarMovies {
   data?: ISimilarMoviesResult[];
@@ -8,7 +9,7 @@ export function SimilarMovies({ data }: ISimilarMovies) {
   return (
     <div className='w-full'>
       <div className='mx-5 flex flex-col gap-4'>
-        <strong className='text-xl'>Você também pode gostar</strong>
+        <SectionTitle title={"Você também pode gostar"} />
       </div>
       <h1>SimilarMovies</h1>
       <h2>{data?.length}</h2>
