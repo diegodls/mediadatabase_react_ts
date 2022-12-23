fe6c983c75526a9ed4c2f4cb8c2fd34efa96dafb
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-23 14:47:36 -0300

    refac: responsive modifications

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ import { Header } from "./components/Header";
 export function App() {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
-      <div className='w-full h-full flex items-center'>
-        <div className='min-w-[640px] max-w-[1366px] flex flex-col items-center'>
-          <Header />
+      <div className='w-full h-full flex flex-col items-center'>
+        <Header />
+        <div className='max-w-CustomMaxWidth'>
           <Outlet />
-          <Footer />
         </div>
+        <Footer />
       </div>
     </ErrorBoundary>
   );
