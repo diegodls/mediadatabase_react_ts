12e3af5b4085bd803d00956608be9d8003d703cf
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-26 10:59:25 -0300

    refac: add background color on title

--- src/components/IUSharedComponents/Section.tsx
+++ src/components/IUSharedComponents/Section.tsx
@@ -6,7 +6,7 @@ export function Section({ title, children }: SectionProps) {
   return (
     <section className='w-full mt-2 px-4'>
       {title ? (
-        <div className='w-full pl-1 border-l-4 border-customColors-red-500'>
+        <div className='w-full pl-1 py-1 border-l-4 border-customColors-red-500 bg-neutral-900'>
           <h2 className='text-xl font-bold'>{title}</h2>
         </div>
       ) : null}
