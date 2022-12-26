6b3d3e54d320f9d0243f9e3eb5fdba88604b1ce9
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-26 14:10:10 -0300

    refac: change title visibility

--- src/components/IUSharedComponents/Section.tsx
+++ src/components/IUSharedComponents/Section.tsx
@@ -1,12 +1,17 @@
 interface SectionProps {
-  title?: string;
   children: React.ReactNode;
+  titleBg?: boolean;
+  title?: string;
 }
-export function Section({ title, children }: SectionProps) {
+export function Section({ title, children, titleBg = true }: SectionProps) {
   return (
     <section className='w-full mt-2 px-4'>
       {title ? (
-        <div className='w-full pl-1 py-1 border-l-4 border-customColors-red-500 bg-neutral-900'>
+        <div
+          className={`w-full pl-1 py-1 border-l-4 border-customColors-red-500 ${
+            titleBg ? "bg-neutral-900" : "md:hidden"
+          }`}
+        >
           <h2 className='text-xl font-bold'>{title}</h2>
         </div>
       ) : null}
