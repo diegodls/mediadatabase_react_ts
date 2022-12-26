45a50abec3f7a9a4cfd95bfbc13ac940c61f3656
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-26 14:09:55 -0300

    refac: better responsive code

--- src/components/TrendingMovieDescription.tsx
+++ src/components/TrendingMovieDescription.tsx
@@ -16,23 +16,27 @@ export function TrendingMovieDescription({
 }: TrendingMovieDescriptionProps) {
   return (
     <>
-      <div className='md:w-80 w-60 ml-16 rounded top-1/2 transform -translate-y-1/2 absolute z-50 overflow-hidden'>
+      <div className='w-full mt-2 px-4 md:mt-0 md:px-0 md:w-80 md:ml-16 md:top-1/2 md:transform md:-translate-y-1/2 md:absolute md:z-50 overflow-hidden'>
         <h1
           aria-label={`Filme: ${slideArray[currentIndex].title}`}
           title={`Filme: ${slideArray[currentIndex].title}`}
-          className='md:text-4xl font-bold wrap-text line-clamp-1 text-2xl md:w-80 md:line-clamp-1'
+          className='md:ext-4xl font-bold wrap-text line-clamp-1 text-2xl md:w-80 md:line-clamp-1'
         >
           {slideArray[currentIndex].title}
         </h1>
         <span
           aria-label={`Resumo do filme: ${slideArray[currentIndex].title}: ${slideArray[currentIndex].overview}`}
           title={slideArray[currentIndex].overview}
-          className='mt-2 md:line-clamp-3 line-clamp-2'
+          className='mt-2 line-clamp-3'
         >
           {slideArray[currentIndex].overview}
         </span>
         <div className='mt-3 flex row items-center justify-between'>
-          <span className='flex row'>
+          <span
+            className='flex row'
+            aria-label='Popularidade'
+            title='Popularidade'
+          >
             <Star className='mt-[2px]' size={24} color='#c00' weight='fill' />
             <span className='text-lg ml-2 font-bold line-clamp-3'>
               {slideArray[currentIndex].vote_average.toFixed(1)}
@@ -56,14 +60,14 @@ export function TrendingMovieDescription({
         {movieGenresList && movieGenresList.genres.length > 0 ? (
           <ul
             aria-label={`Lista dos gêneros do filme:  ${slideArray[currentIndex].title}`}
-            className='mt-3 md:flex flex-wrap gap-x-2 row hidden'
+            className='mt-3 flex flex-wrap gap-x-2 row'
           >
             {movieGenresList.genres.map((genre, id: number) => {
               return slideArray[currentIndex].genre_ids
                 .slice(0, 5)
                 .includes(genre.id) ? (
                 <li
-                  key={id}
+                  key={genre.id}
                   title={genre.name}
                   aria-label={genre.name}
                   className='mb-1 flex bg-black/10 rounded-md border-2 border-customColors-red-500 cursor-default'
