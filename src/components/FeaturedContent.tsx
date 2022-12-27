390dfddc70d62e04e2232a54929d962585a8b458
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-27 14:21:08 -0300

    add: move repeated code to a new component

---
+++ src/components/FeaturedContent.tsx
@@ -0,0 +1,70 @@
+import { IGenres } from "../interfaces/IGenres";
+import { API_BASEURL_IMAGE_1280 } from "../utils/constants";
+import { FeaturedContentDescription } from "./FeaturedContentDescription";
+
+import backdrop_path_error from "../assets/img/image-error.png";
+
+export interface FeaturedContentProps {
+  genresList?: IGenres;
+  contentGenresList?: number[];
+  contentID?: number;
+  title?: string;
+  subTitle?: string;
+  backdrop_path?: string | null;
+  overview?: string;
+  vote_average?: number;
+  runtime?: number;
+  type?: string;
+  showReadMore?: boolean;
+}
+
+export function FeaturedContent({
+  genresList,
+  contentGenresList,
+  contentID,
+  title,
+  subTitle,
+  backdrop_path,
+  overview,
+  vote_average,
+  runtime,
+  type,
+  showReadMore,
+}: FeaturedContentProps) {
+  return (
+    <section className='relative'>
+      <div className='w-full max-h-[80vh] flex items-center relative overflow-hidden'>
+        <div className='w-full h-full top-0 bg-gradient-to-r from-customColors-background via-transparent absolute z-40 hidden md:block' />
+
+        <div className='w-full h-12 bottom-0 bg-gradient-to-t from-customColors-background absolute z-50' />
+
+        <img
+          className='w-full h-auto flex-shrink-0 select-none bg-cover absolute z-20'
+          src={API_BASEURL_IMAGE_1280 + backdrop_path}
+          alt={title}
+          title={title}
+        />
+
+        <img
+          className='min-w-full min-h-full flex-shrink-0 select-none relative z-10'
+          src={backdrop_path_error}
+          alt={`Erro ao carregar a imagem de ${title}`}
+          title={`Erro ao carregar a imagem de ${title}`}
+        />
+      </div>
+
+      <FeaturedContentDescription
+        genresList={genresList}
+        contentGenresList={contentGenresList}
+        contentID={contentID}
+        title={title}
+        subTitle={subTitle}
+        overview={overview}
+        vote_average={vote_average}
+        type={type}
+        runtime={runtime}
+        showReadMore={showReadMore}
+      />
+    </section>
+  );
+}
---
+++ src/components/FeaturedContentDescription.tsx
@@ -0,0 +1,97 @@
+import { Star } from "phosphor-react";
+import { FeaturedContentProps } from "./FeaturedContent";
+
+export function FeaturedContentDescription({
+  genresList,
+  contentGenresList,
+  contentID,
+  title,
+  subTitle,
+  overview,
+  vote_average,
+  runtime,
+  type,
+  showReadMore,
+}: FeaturedContentProps) {
+  const mediaType: string = type === "movie" ? "Filme" : "Seriado";
+  const urlType: string = type === "movie" ? "movie" : "tvshow";
+
+  return (
+    <div className='mt-2 px-4 md:max-w-[40%] md:px-0 md:mt-0 md:ml-16 md:left-0 md:top-1/2 md:transform md:-translate-y-1/2 md:absolute md:z-50 md:overflow-hidden'>
+      {title ? (
+        <h1
+          aria-label={`${mediaType}: ${title}`}
+          title={`${mediaType}: ${title}`}
+          className='md:text-4xl font-bold wrap-text text-2xl'
+        >
+          {title}
+        </h1>
+      ) : null}
+
+      {subTitle ? (
+        <p
+          aria-label={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
+          title={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
+          className='font-bold wrap-text text-md'
+        >
+          {subTitle}
+        </p>
+      ) : null}
+
+      {overview ? (
+        <span
+          aria-label={`Resumo do ${mediaType}: ${title}: ${overview}`}
+          title={overview}
+          className='mt-2 md:line-clamp-3 line-clamp-2'
+        >
+          {overview}
+        </span>
+      ) : null}
+
+      <div className='mt-2 flex row items-center justify-between'>
+        {vote_average ? (
+          <span className='flex row'>
+            <Star className='mt-[2px]' size={24} color='#c00' weight='fill' />
+            <span className='text-lg ml-2 font-bold line-clamp-3'>
+              {vote_average?.toFixed(1)}
+            </span>
+          </span>
+        ) : null}
+
+        {runtime ? (
+          <p className='text-lg ml-10 font-bold line-clamp-3'>{runtime} min</p>
+        ) : null}
+
+        {showReadMore && contentID ? (
+          <a
+            aria-label={`Botão: Clique para saber mais sobre ${title}`}
+            href={`${urlType}/${contentID}`}
+            className='min-w-auto flex items-center justify-center px-2 bg-customColors-red-500 rounded'
+          >
+            <span className='p-1 m-auto text-white'>Leia Mais</span>
+          </a>
+        ) : null}
+      </div>
+
+      {genresList && genresList.genres.length > 0 ? (
+        <ul
+          aria-label={`Lista dos gêneros do ${mediaType}: ${title}`}
+          className='mt-3 md:flex flex-wrap gap-x-2 row hidden'
+        >
+          {genresList.genres.map((genre) => {
+            return contentGenresList?.slice(0, 5).includes(genre.id) ? (
+              <li
+                key={genre.id}
+                title={genre.name}
+                aria-label={genre.name}
+                className='mb-1 flex bg-black/20 rounded-md border-2 border-customColors-red-500 cursor-default'
+              >
+                <p className='m-auto p-1'>{genre.name}</p>
+              </li>
+            ) : null;
+          })}
+        </ul>
+      ) : null}
+    </div>
+  );
+}
