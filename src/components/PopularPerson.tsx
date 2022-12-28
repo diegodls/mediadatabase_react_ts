bf7716f27a1cbeb0c02c261d8e4322911ab0d06f
DiegoL <contato.diegoluisdasilva@gmail.com>
2022-12-28 11:56:43 -0300

    refac: apply pattern (Section)

--- src/components/PopularPerson.tsx
+++ src/components/PopularPerson.tsx
@@ -2,6 +2,7 @@ import { IPerson } from "../interfaces/IPerson";
 import { API_BASEURL_IMAGE_200 } from "../utils/constants";
 
 import profile_picture_not_found from "../assets/img/profile_picture_not_found.png";
+import { Section } from "./Section";
 interface PopularPersonProps {
   personList: IPerson[] | undefined;
 }
@@ -9,29 +10,34 @@ export function PopularPerson({ personList }: PopularPersonProps) {
   return (
     <>
       {personList && personList?.length > 0 ? (
-        <div className='w-full flex flex-row justify-around items-center mt-2'>
-          {personList.slice(0, 5).map((person: IPerson, _) => (
-            <div
-              key={person.id}
-              className='h-full flex flex-col items-center justify-center p-2 rounded hover:bg-zinc-800 cursor-pointer transition-all'
-            >
-              <div className='w-32 h-32 rounded-full overflow-hidden'>
-                {person.profile_path ? (
-                  <img
-                    src={`${API_BASEURL_IMAGE_200}${person.profile_path}`}
-                    alt={`Foto de ${person.name}`}
-                  />
-                ) : (
-                  <img
-                    src={`${profile_picture_not_found}`}
-                    alt={`Foto de ${person.name} não foi encontrada.`}
-                  />
-                )}
-              </div>
-              <strong>{person.name}</strong>
-            </div>
-          ))}
-        </div>
+        <Section title='Populares'>
+          <ul className='w-full flex flex-row flex-wrap gap-2 justify-around'>
+            {personList.slice(0, 5).map((person: IPerson, _) => (
+              <li
+                key={person.id}
+                title={`Visitar o perfil de ${person.name}`}
+                className='flex flex-col items-center  bg-neutral-800 rounded transform scale-100 hover:scale-105 hover:drop-shadow-xl hover:z-10 hover:bg-zinc-800 cursor-pointer transition-all overflow-hidden'
+              >
+                <div>
+                  {person.profile_path ? (
+                    <img
+                      src={`${API_BASEURL_IMAGE_200}${person.profile_path}`}
+                      alt={`Foto de ${person.name}`}
+                      className='h-52'
+                    />
+                  ) : (
+                    <img
+                      src={`${profile_picture_not_found}`}
+                      alt={`Foto de ${person.name} não foi encontrada.`}
+                      className='h-52'
+                    />
+                  )}
+                </div>
+                <p className='font-bold line-clamp-1'>{person.name}</p>
+              </li>
+            ))}
+          </ul>
+        </Section>
       ) : null}
     </>
   );
