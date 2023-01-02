c722f153a2a57e6e00fda741b84a82c6becd9779
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-01-02 12:48:56 -0300

    refac: build base component

--- src/components/SimilarMovies.tsx
+++ src/components/SimilarMovies.tsx
@@ -1,15 +1,88 @@
+import { useEffect, useRef, useState } from "react";
 import { ISimilarMoviesResult } from "../interfaces/ISimilarMovies";
+import { HomeListItem } from "./HomeListItem";
 import { Section } from "./Section";
 
 interface ISimilarMovies {
-  data?: ISimilarMoviesResult[];
+  similarMovies: ISimilarMoviesResult[] | undefined;
 }
 
-export function SimilarMovies({ data }: ISimilarMovies) {
-  return (
+export function SimilarMovies({ similarMovies }: ISimilarMovies) {
+  const listRef = useRef<HTMLUListElement>(null);
+  const listHeightShowSize = 208; // h-52 or 13rem
+
+  const [listHeight, setListHeight] = useState<number | undefined>(undefined);
+  const [showMore, setShowMore] = useState<boolean>(false);
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+
+  function handleResize() {
+    if (
+      listRef.current?.scrollHeight &&
+      listRef.current?.scrollHeight > listHeightShowSize
+    ) {
+      setShowMore(true);
+    }
+
+    setListHeight(listRef.current?.scrollHeight);
+  }
+
+  function handleCollapse() {
+    setIsCollapsed((prev) => !prev);
+
+    if (!isCollapsed) {
+      setTimeout(() => {
+        listRef.current?.scrollIntoView();
+      }, 250);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    setTimeout(() => {
+      setListHeight(listRef.current?.scrollHeight);
+      handleResize();
+    }, 250);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return similarMovies && similarMovies.length > 0 ? (
     <Section title='Você também pode gostar'>
-      <h1>SimilarMovies</h1>
-      <h2>{data?.length}</h2>
+      <div
+        className={`w-full ${
+          isCollapsed ? "max-h-auto" : "max-h-52"
+        } justify-center overflow-hidden`}
+      >
+        <ul
+          ref={listRef}
+          className='w-full h-full flex flex-row flex-wrap justify-center'
+        >
+          {similarMovies.map((similarMovie, _) => {
+            return (
+              <li key={similarMovie.id} className='h-52 flex'>
+                <HomeListItem
+                  title={similarMovie.title || similarMovie.original_title}
+                  poster_path={similarMovie.poster_path}
+                  type={"movie"}
+                  contentID={similarMovie.id}
+                />
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <button
+        className={`w-full h-8 flex justify-center bg-gradient-to-t from-black`}
+        onClick={() => {
+          handleCollapse();
+        }}
+      >
+        <strong>{isCollapsed ? "Mostrar Menos" : "Mostrar Mais"}</strong>
+      </button>
     </Section>
-  );
+  ) : null;
 }
