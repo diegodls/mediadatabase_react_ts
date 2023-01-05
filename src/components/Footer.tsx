6af885dc92ad36096ae060ac7039a6f6dc8ad9c9
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-01-05 15:02:06 -0300

    refac: change increase size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,20 @@
 export function Footer() {
   return (
-    <footer className='w-full h-9 flex items-center justify-center mt-2 bg-zinc-800'>
-      <strong className='cursor-default'>
-        Feito com <div className='inline-block hover:animate-ping'>❤️</div> por{" "}
-        <a
-          href='https://github.com/diegodls'
-          target='_blank'
-          rel='external'
-          className='hover:text-customColors-red-500 transition-all hover:animate-pulse cursor-pointer'
-        >
-          diegodls
-        </a>
-      </strong>
+    <footer className='w-full mt-2 bg-zinc-800'>
+      <div className='w-full h-10 flex items-center justify-center '>
+        <strong className='cursor-default'>
+          Feito com <div className='inline-block hover:animate-ping'>❤️</div>{" "}
+          por{" "}
+          <a
+            href='https://github.com/diegodls'
+            target='_blank'
+            rel='external'
+            className='hover:text-customColors-red-500 transition-all hover:animate-pulse cursor-pointer'
+          >
+            diegodls
+          </a>
+        </strong>
+      </div>
     </footer>
   );
 }
