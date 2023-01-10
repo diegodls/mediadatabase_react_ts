7bdef3b8076a4745d04c3b421c8a25d1131bb61c
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-01-10 16:09:42 -0300

    refac: hide subtitle if = title & margin left

--- src/components/FeaturedContentDescription.tsx
+++ src/components/FeaturedContentDescription.tsx
@@ -51,98 +51,99 @@ export function FeaturedContentDescription({
           {title}
         </h1>
       ) : null}
-
-      {subTitle ? (
-        <p
-          aria-label={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
-          title={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
-          className='font-bold wrap-text text-md'
-        >
-          {subTitle}
-        </p>
-      ) : null}
-
-      {overview && showInfo ? (
-        <span
-          aria-label={`Resumo do ${mediaType}: ${title}: ${overview}`}
-          title={overview}
-          className='mt-2 md:line-clamp-3 line-clamp-2'
-        >
-          {overview}
-        </span>
-      ) : null}
-
-      <div className='w-full mt-2 flex items-center justify-between'>
-        {release_date ? (
-          <span
-            aria-label={`O ${mediaType} ${title} foi lançado em ${formatDate(
-              release_date
-            )}`}
-            title={`O ${mediaType} ${title} foi lançado em ${formatDate(
-              release_date
-            )}`}
-            className='text-lg font-bold line-clamp-3'
-          >
-            {release_date.split("-")[0]}
-          </span>
-        ) : null}
-
-        {runtime ? (
+      <div className='ml-2'>
+        {subTitle && title != subTitle ? (
           <p
-            aria-label={`O ${mediaType} ${title} tem a duração de ${runtime} minutos`}
-            title={`O ${mediaType} ${title} tem a duração de ${runtime} minutos`}
-            className='text-lg font-bold line-clamp-3'
+            aria-label={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
+            title={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
+            className='font-bold wrap-text text-md'
           >
-            {runtime} min
+            {subTitle}
           </p>
         ) : null}
 
-        {vote_average ? (
+        {overview && showInfo ? (
           <span
-            aria-label={`O ${mediaType} ${title} tem uam popularidade de ${vote_average}`}
-            title={`O ${mediaType} ${title} tem uam popularidade de ${vote_average}s`}
-            className='flex'
+            aria-label={`Resumo do ${mediaType}: ${title}: ${overview}`}
+            title={overview}
+            className='mt-2 md:line-clamp-3 line-clamp-2'
           >
-            <Star
-              size={24}
-              weight='fill'
-              className='text-customColors-red-500'
-            />
-            <span className='ml-2 text-lg font-bold line-clamp-3'>
-              {vote_average?.toFixed(1)}
-            </span>
+            {overview}
           </span>
         ) : null}
 
-        {showReadMore && contentID ? (
-          <NavLink
-            to={`${type}/${contentID}`}
-            className='min-w-auto flex items-center justify-center px-2 bg-customColors-red-500 rounded'
+        <div className='w-full mt-2 flex items-center justify-between'>
+          {release_date ? (
+            <span
+              aria-label={`O ${mediaType} ${title} foi lançado em ${formatDate(
+                release_date
+              )}`}
+              title={`O ${mediaType} ${title} foi lançado em ${formatDate(
+                release_date
+              )}`}
+              className='text-lg font-bold line-clamp-3'
+            >
+              {release_date.split("-")[0]}
+            </span>
+          ) : null}
+
+          {runtime ? (
+            <p
+              aria-label={`O ${mediaType} ${title} tem a duração de ${runtime} minutos`}
+              title={`O ${mediaType} ${title} tem a duração de ${runtime} minutos`}
+              className='text-lg font-bold line-clamp-3'
+            >
+              {runtime} min
+            </p>
+          ) : null}
+
+          {vote_average ? (
+            <span
+              aria-label={`O ${mediaType} ${title} tem uam popularidade de ${vote_average}`}
+              title={`O ${mediaType} ${title} tem uam popularidade de ${vote_average}s`}
+              className='flex'
+            >
+              <Star
+                size={24}
+                weight='fill'
+                className='text-customColors-red-500'
+              />
+              <span className='ml-2 text-lg font-bold line-clamp-3'>
+                {vote_average?.toFixed(1)}
+              </span>
+            </span>
+          ) : null}
+
+          {showReadMore && contentID ? (
+            <NavLink
+              to={`${type}/${contentID}`}
+              className='min-w-auto flex items-center justify-center px-2 bg-customColors-red-500 rounded'
+            >
+              <p className='p-1 m-auto text-white'>Leia Mais</p>
+            </NavLink>
+          ) : null}
+        </div>
+
+        {genresList && genresList.genres.length > 0 ? (
+          <ul
+            aria-label={`Lista dos gêneros do ${mediaType}: ${title}`}
+            className='mt-3 flex flex-wrap gap-x-2 row '
           >
-            <p className='p-1 m-auto text-white'>Leia Mais</p>
-          </NavLink>
+            {genresList.genres.map((genre) => {
+              return contentGenresList?.slice(0, 5).includes(genre.id) ? (
+                <li
+                  key={genre.id}
+                  title={genre.name}
+                  aria-label={genre.name}
+                  className='mb-1 flex bg-black/20 rounded-md border-2 border-customColors-red-500 cursor-default'
+                >
+                  <p className='m-auto p-1'>{genre.name}</p>
+                </li>
+              ) : null;
+            })}
+          </ul>
         ) : null}
       </div>
-
-      {genresList && genresList.genres.length > 0 ? (
-        <ul
-          aria-label={`Lista dos gêneros do ${mediaType}: ${title}`}
-          className='mt-3 flex flex-wrap gap-x-2 row '
-        >
-          {genresList.genres.map((genre) => {
-            return contentGenresList?.slice(0, 5).includes(genre.id) ? (
-              <li
-                key={genre.id}
-                title={genre.name}
-                aria-label={genre.name}
-                className='mb-1 flex bg-black/20 rounded-md border-2 border-customColors-red-500 cursor-default'
-              >
-                <p className='m-auto p-1'>{genre.name}</p>
-              </li>
-            ) : null;
-          })}
-        </ul>
-      ) : null}
     </div>
   );
 }
