6c255e45df34c798d6d6e196e3d47db677ae5590
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-01-19 11:51:50 -0300

    testing

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -2,16 +2,13 @@ export function Test() {
   const imageError =
     "https://www.salonlfc.com/wp-content/uploads/2018/01/image-not-found-scaled.png";
   return (
-    <div className='bg-red-500 w-screen h-screen flex items-center justify-center p-11'>
-      <div
-        className={`max-h-[500px] flex items-center justify-center overflow-hidden bg-green-500`}
-      >
-        <img
-          className='min-w-full min-h-full select-none relative z-10'
-          src={imageError}
-          alt={`Carregando...`}
-          title={`Carregando...`}
-        />
+    <div className='w-full h-full flex flex-col items-center justify-center p-11 bg-red-500'>
+      <div className='w-full h-[400px] bg-green-500'>
+        <h1>AAAAAAAAAA</h1>
+      </div>
+
+      <div className='w-full h-[400px] bg-blue-500'>
+        <h1>BBBBBBBBBB</h1>
       </div>
     </div>
   );
