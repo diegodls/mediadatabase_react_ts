5acd56d97a78a874b23167f560998c13506a3976
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-01-24 11:39:44 -0300

    refac: testing....

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,24 +1,31 @@
 export function Test() {
-  const imageError = "https://source.unsplash.com/random/800x600";
+  const imageRandomMock =
+    "https://images.unsplash.com/photo-1672420877594-99a77c4dff5c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=600&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NDQ5Nzk4MQ&ixlib=rb-4.0.3&q=80&w=800";
 
   const imageRandom = "https://source.unsplash.com/random/800x600";
+
+  const actualImage = imageRandomMock;
   return (
-    <div className='w-full h-[500px] mt-12 flex flex-col items-center p-2 bg-red-500'>
-      <p className=''>
-        Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam
-        a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei o pau no
-        gatis, per gatis num morreus. Copo furadis é disculpa de bebadis, arcu
-        quam euismod magna. Si num tem leite então bota uma pinga aí cumpadi!
-        Diuretics paradis num copo é motivis de denguis. Todo mundo vê os porris
-        que eu tomo, mas ninguém vê os tombis que eu levo! Mais vale um bebadis
-        conhecidiss, que um alcoolatra anonimis. Casamentiss faiz malandris se
-        pirulitá. A ordem dos tratores não altera o pão duris. Nullam volutpat
-        risus nec leo commodo, ut interdum diam laoreet. Sed non consequat odio.
-        Per aumento de cachacis, eu reclamis. Nec orci ornare consequat.
-        Praesent lacinia ultrices consectetur. Sed non ipsum felis.
-      </p>
-      <div className='w-full flex-1 relative overflow-auto bg-green-800'>
-        <img src={imageRandom} alt='teste' className='h-full' />
+    <div className='w-full flex flex-col items-center overflow-hidden'>
+      <div className='overflow-hidden relative '>
+        <img src={actualImage} alt='teste' className='w-[200px] h-[200px]' />
+        <p className=''>
+          Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius.
+          Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Atirei
+          o pau no gatis, per gatis num morreus. Copo furadis é disculpa de
+          bebadis, arcu quam euismod magna. Si num tem leite então bota uma
+          pinga aí cumpadi! Diuretics paradis num copo é motivis de denguis.
+          Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que eu
+          levo! Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.
+          Casamentiss faiz malandris se pirulitá. A ordem dos tratores não
+          altera o pão duris. Nullam volutpat risus nec leo commodo, ut interdum
+          diam laoreet. Sed non consequat odio. Per aumento de cachacis, eu
+          reclamis. Nec orci ornare consequat. Praesent lacinia ultrices
+          consectetur. Sed non ipsum felis.
+        </p>
+      </div>
+      <div className='flex-1 relative overflow-auto bg-green-800'>
+        <img src={actualImage} alt='teste' className='h-full' />
       </div>
     </div>
   );
