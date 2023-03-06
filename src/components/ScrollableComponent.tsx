1544a9003b25342524d5321f9ff813d1a05fbb84
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-03-06 16:12:11 -0300

    refac: change scroll type for better control

--- src/components/ScrollableComponent.tsx
+++ src/components/ScrollableComponent.tsx
@@ -11,6 +11,7 @@ interface IListRowProps {
 export function ScrollableComponent({ children, title, error }: IListRowProps) {
   const listRef = useRef<HTMLDivElement>(null);
   const BUTTON_WIDTH = 36; //in pixels
+  const LIST_ITEM_AMOUNT = 20;
 
   const [isMouseOverList, setIsMouseOverList] = useState<boolean>(false);
   const [isScrollable, setIsScrollable] = useState<boolean>(false);
@@ -23,25 +24,57 @@ export function ScrollableComponent({ children, title, error }: IListRowProps) {
       : setIsScrollable(false);
   }
 
-  function handleScrollLeft(
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  function handleScroll(
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    type: "LEFT" | "RIGHT"
   ) {
     e.preventDefault();
     handleResizeWindow();
-    if (!listRef.current) return;
-    listRef.current.scrollLeft -= listRef.current
-      ? listRef.current?.offsetWidth
-      : 0;
-  }
 
-  function handleScrollRight(
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) {
-    e.preventDefault();
-    handleResizeWindow();
+    console.log("");
+    console.log("handleScroll");
+
     if (!listRef.current) return;
 
-    listRef.current.scrollLeft += listRef.current?.offsetWidth;
+    const ITEM_HEIGHT = listRef.current?.scrollWidth / LIST_ITEM_AMOUNT;
+
+    const currentDistance = listRef.current?.getBoundingClientRect().x;
+
+    let moveTo = 0;
+
+    console.log("_____ANTES");
+    console.log({
+      type,
+      moveTo,
+      ITEM_HEIGHT,
+      currentDistance,
+    });
+
+    if (type === "LEFT") {
+      moveTo = ITEM_HEIGHT + currentDistance;
+      console.log(`_____moveTo LEFT: ${moveTo}`);
+    }
+
+    if (type === "RIGHT") {
+      moveTo = -ITEM_HEIGHT + currentDistance;
+      console.log(`__________moveTo RIGHT: ${moveTo}`);
+    }
+
+    if (moveTo >= 0 || moveTo < currentDistance - ITEM_HEIGHT) {
+      console.log(`__________moveTo 0: ${moveTo}`);
+
+      moveTo = 0;
+    }
+
+    listRef.current.style.transform = `translateX(${moveTo}px)`;
+
+    console.log("_____DEPOIS");
+    console.log({
+      type,
+      moveTo,
+      ITEM_HEIGHT,
+      currentDistance,
+    });
   }
 
   useEffect(() => {
@@ -76,7 +109,7 @@ export function ScrollableComponent({ children, title, error }: IListRowProps) {
                 <button
                   aria-label='Scroll para esquerda'
                   title='Scroll para esquerda'
-                  onClick={handleScrollLeft}
+                  onClick={(e) => handleScroll(e, "LEFT")}
                   className={`w-[${BUTTON_WIDTH}px] h-full flex items-center justify-center scale-y-90 bg-black/20 hover:bg-black/80 rounded-r-sm overflow-hidden absolute left-0 z-50 cursor-pointer transition-all select-none ${
                     isMouseOverList ? "opacity-100" : "opacity-0"
                   }`}
@@ -86,7 +119,7 @@ export function ScrollableComponent({ children, title, error }: IListRowProps) {
                 <button
                   aria-label='Scroll para direita'
                   title='Scroll para direita'
-                  onClick={handleScrollRight}
+                  onClick={(e) => handleScroll(e, "RIGHT")}
                   className={`w-[${BUTTON_WIDTH}px] h-full flex items-center justify-center scale-y-90 bg-black/50 hover:bg-black/80 rounded-l-sm overflow-hidden absolute right-0 z-50 cursor-pointer transition-all select-none  ${
                     isMouseOverList ? "opacity-100" : "opacity-0"
                   }`}
@@ -97,12 +130,16 @@ export function ScrollableComponent({ children, title, error }: IListRowProps) {
             ) : null}
 
             <div
-              className={`w-full h-full overflow-x-scroll hide-scrollbar scroll-smooth overflow-hidden ${
+              className={`w-full h-full hide-scrollbar scroll-smooth overflow-hidden overflow-x-scroll ${
                 !isScrollable ? "flex justify-center" : ""
               }`}
-              ref={listRef}
             >
-              <div className='w-auto h-full'>{children}</div>
+              <div
+                className='w-auto h-full hide-scrollbar scroll-smooth transition-all'
+                ref={listRef}
+              >
+                {children}
+              </div>
             </div>
           </div>
         </div>
