9b8853dc0580b6135f93a3b839527bcbdf687607
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-03-09 16:39:19 -0300

    style: change positions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,6 @@ import { SearchInput } from "./SearchInput";
 
 export function Header() {
   const location = useLocation();
-  console.log(location.pathname);
 
   const navData: INavData[] = [
     { title: "HOME", to: "/" },
@@ -14,30 +13,27 @@ export function Header() {
   ];
 
   return (
-    <header
-      className={`w-full h-headerHeight flex flex-row items-center justify-center absolute z-50 overflow-hidden transition-all`}
-    >
-      <div className='w-full h-full  max-w-CustomMaxWidth  px-2 flex items-center justify-between z-50'>
-        <span className='max-w-[4em] sm:max-w-[7em] h-full flex items-center justify-center overflow-hidden'>
-          {/* LOGO */}
-        </span>
+    <header className='w-full h-headerHeight px-layoutX flex flex-row items-center absolute z-50 overflow-hidden transition-all'>
+      <span className='w-80 max-w-[4em] sm:max-w-[7em] h-full flex items-center justify-center overflow-hidden bg-yellow-700'>
+        {/* LOGO */}
+      </span>
+
+      <nav className='h-full flex justify-center ml-auto mr-auto left-0 right-0 text-center absolute'>
+        {navData.map((item, _) => (
+          <CustomNavLink
+            key={`${item.to + item.title}`}
+            title={item.title}
+            to={item.to}
+            currentPath={location.pathname}
+          />
+        ))}
+      </nav>
+
+      <div className='mr-layoutX right-0 absolute'>
         <SearchInput />
       </div>
 
-      <div className='h-full w-full flex flex-row items-center justify-center gap-5 absolute z-50'>
-        <nav className='h-full flex items-center justify-center'>
-          {navData.map((item, _) => (
-            <CustomNavLink
-              key={`${item.to + item.title}`}
-              title={item.title}
-              to={item.to}
-              currentPath={location.pathname}
-            />
-          ))}
-        </nav>
-      </div>
-
-      <div className='w-full h-12 top-0 bg-gradient-to-b from-customColors-background/80 absolute z-10' />
+      <div className='w-full h-12 top-0 bg-gradient-to-b from-customColors-background/80 absolute -z-10' />
     </header>
   );
 }
