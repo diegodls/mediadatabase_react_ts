be392360eb876dc06ad16e9d1806fc2f7cc35af4
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-03-21 15:52:34 -0300

    refac: add random movie component

--- src/hooks/useGetRandomMovie.ts
+++ src/hooks/useGetRandomMovie.ts
@@ -1,21 +1,40 @@
 import { useEffect, useState } from "react";
 import { IErrorFetchContent } from "../interfaces/IErrorFetchContent";
 
+import {
+  IDiscoveryMovies,
+  IDiscoveryMoviesResult,
+} from "../interfaces/IDiscoveryMovies";
+import { IGenres } from "../interfaces/IGenres";
+import { Genre } from "../interfaces/IMovieDetails";
 import { service } from "../services/api";
 
-export function useGetRandomMovie<T>(url: string) {
-  const [data, setData] = useState<T>();
-  const [loadingData, setLoadingData] = useState<boolean>(true);
-  const [dataError, setDataError] = useState<IErrorFetchContent>();
+export function useGetRandomMovie<T>() {
+  const URL_DISCOVERY_BY_GENRES =
+    "discover/movie?sort_by=popularity.desc&include_adult=false&page=1&with_genres=";
 
-  async function fetchData() {
-    setLoadingData(true);
-    setDataError(undefined);
+  const [randomMovie, setRandomMovie] = useState<IDiscoveryMoviesResult>();
+  const [loadingRandomMovie, setLoadingRandomMovie] = useState<boolean>(true);
+  const [randomMovieError, setRandomMovieError] =
+    useState<IErrorFetchContent>();
+
+  async function getRandomMovie() {
+    setLoadingRandomMovie(true);
+    setRandomMovieError(undefined);
 
     let isCurrentUrl: boolean = true;
 
-    if (!url || url === undefined || url.length <= 0) {
-      setDataError({
+    let randomGenre: Genre = {
+      id: 28,
+      name: "Ação",
+    };
+
+    if (
+      !URL_DISCOVERY_BY_GENRES ||
+      URL_DISCOVERY_BY_GENRES === undefined ||
+      URL_DISCOVERY_BY_GENRES.length <= 0
+    ) {
+      setRandomMovieError({
         status_message: "É necessário informar a URL do conteúdo!",
         success: false,
         status_code: 404,
@@ -23,33 +42,85 @@ export function useGetRandomMovie<T>(url: string) {
       return;
     }
 
-    await service
-      .get<T>(`${url}`)
+    const moviesGenres: IGenres | undefined = await service
+      .get<IGenres>(`genre/movie/list`)
       .then((response) => {
         if (response.data && isCurrentUrl) {
-          setData(response.data);
+          return response.data;
         }
       })
       .catch((error: IErrorFetchContent) => {
-        setDataError(error);
-      })
-      .finally(() => {
-        setLoadingData(false);
+        console.log(
+          `Erro ao localizar os gêneros dos filmes: ${error.status_message}`
+        );
+        throw new Error(error.status_message);
       });
 
+    if (moviesGenres && moviesGenres.genres.length > 0) {
+      let randomNumberForGenres = Math.floor(
+        Math.random() * moviesGenres.genres.length
+      );
+
+      randomGenre = moviesGenres.genres[randomNumberForGenres];
+      console.log(`randomGenre`);
+      console.log(randomGenre);
+    }
+    console.log(`${URL_DISCOVERY_BY_GENRES}${randomGenre.id}`);
+
+    const moviesByRandomGenre: IDiscoveryMoviesResult[] | undefined =
+      await service
+        .get<IDiscoveryMovies>(`${URL_DISCOVERY_BY_GENRES}${randomGenre.id}`)
+        .then((response) => {
+          if (response.data.results && isCurrentUrl) {
+            return response.data.results;
+          }
+        })
+        .catch((error: IErrorFetchContent) => {
+          console.log(
+            `Erro ao localizar os filmes pelo gênero: ${randomGenre.id} - ${randomGenre.name} : ${error.status_message}`
+          );
+          throw new Error(error.status_message);
+        });
+
+    if (moviesByRandomGenre && moviesByRandomGenre.length > 0) {
+      let randomMovieValid = {} as IDiscoveryMoviesResult;
+
+      while (!randomMovieValid.overview) {
+        let randomNumberFromGenres = Math.floor(
+          Math.random() * moviesByRandomGenre.length
+        );
+
+        console.log(`randomNumberForGenres: ${randomNumberFromGenres}`);
+        console.log(
+          `${moviesByRandomGenre[randomNumberFromGenres].original_title} - ${moviesByRandomGenre[randomNumberFromGenres].genre_ids}`
+        );
+        if (
+          moviesByRandomGenre[randomNumberFromGenres].overview &&
+          moviesByRandomGenre[randomNumberFromGenres].poster_path &&
+          (moviesByRandomGenre[randomNumberFromGenres].title ||
+            moviesByRandomGenre[randomNumberFromGenres].original_title)
+        ) {
+          randomMovieValid = moviesByRandomGenre[randomNumberFromGenres];
+        }
+      }
+
+      setRandomMovie(randomMovieValid);
+    }
+
+    setLoadingRandomMovie(false);
     return () => {
       isCurrentUrl = false;
     };
   }
 
   useEffect(() => {
-    fetchData();
+    getRandomMovie();
   }, []);
 
   return {
-    data,
-    loadingData,
-    dataError,
-    fetchData,
+    randomMovie,
+    loadingRandomMovie,
+    randomMovieError,
+    getRandomMovie,
   };
 }
--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { FeaturedContent } from "../components/FeaturedContent";
 import { List } from "../components/List";
 import { Loading } from "../components/Loading";
 import { useFetchData } from "../hooks/useFetchData";
+import { useGetRandomMovie } from "../hooks/useGetRandomMovie";
 import {
   IDiscoveryMovies,
   IDiscoveryMoviesResult,
@@ -18,88 +19,39 @@ export function Movie() {
 
   const { data: movieGenresList } = useFetchData<IGenres>(`genre/movie/list`);
 
+  const { randomMovie, loadingRandomMovie, randomMovieError } =
+    useGetRandomMovie();
+
   const {
     data: actionMovies,
     loadingData: loadingActionMovies,
     dataError: errorActionMovies,
-    fetchData: fetchActionMovies,
   } = useFetchData<IDiscoveryMovies>(`${URL_DISCOVERY_BY_GENRES}28`);
 
   const {
     data: adventureMovies,
     loadingData: loadingAdventureMovies,
     dataError: errorAdventureMovies,
-    fetchData: fetchAdventureMovies,
   } = useFetchData<IDiscoveryMovies>(`${URL_DISCOVERY_BY_GENRES}12`);
-
-  const FEATURED_MOVIE_OPTIONS: (IDiscoveryMovies | undefined)[] = [
-    actionMovies,
-    adventureMovies,
-  ];
-
-  const [featuredMovie, setFeaturedMovie] = useState<IDiscoveryMoviesResult>();
 
   useEffect(() => {
     document.title = `MDB - Filmes`;
   }, []);
 
-  useEffect(() => {
-    console.log(`${"@".repeat(50)}useEffect setFeaturedMovie`);
-    if (featuredMovie) {
-      console.log("Temmm");
-
-      console.log(featuredMovie);
-
-      return;
-    }
-
-    let featuredItemFromOptions: IDiscoveryMoviesResult | undefined = undefined;
-    let randomNumberOfTypes: number = 0;
-    let randomNumberOfMovies: number = 0;
-
-    if (actionMovies || adventureMovies) {
-      randomNumberOfTypes = Math.ceil(
-        Math.random() * FEATURED_MOVIE_OPTIONS.length
-      );
-
-      if (FEATURED_MOVIE_OPTIONS[randomNumberOfTypes]?.results) {
-        const quantityOfMoviesByType =
-          FEATURED_MOVIE_OPTIONS[randomNumberOfTypes]?.results.length || 20;
-
-        randomNumberOfMovies = Math.ceil(
-          Math.random() * quantityOfMoviesByType
-        );
-      }
-
-      featuredItemFromOptions =
-        FEATURED_MOVIE_OPTIONS[randomNumberOfTypes]?.results[
-          randomNumberOfMovies
-        ];
-
-      console.log(`randomNumberOfTypes: ${randomNumberOfTypes}`);
-      console.log(`randomNumberOfMovies: ${randomNumberOfMovies}`);
-
-      console.log(featuredItemFromOptions?.title);
-
-      setFeaturedMovie(featuredItemFromOptions);
-    }
-  }, [FEATURED_MOVIE_OPTIONS]);
-
-  return !loadingActionMovies && !loadingAdventureMovies ? (
+  return !loadingActionMovies &&
+    !loadingAdventureMovies &&
+    !loadingRandomMovie ? (
     <div className='w-full mt-headerHeight'>
-      <p className='w-full h-12 flex items-center justify-start bg-gray-800 text-red-600'>
-        FEATURED_MOVIE_OPTIONS: {FEATURED_MOVIE_OPTIONS.length}
-      </p>
       <FeaturedContent
         genresList={movieGenresList}
-        contentGenresList={featuredMovie?.genre_ids}
-        contentID={featuredMovie?.id}
-        title={featuredMovie?.title}
-        subTitle={featuredMovie?.original_title}
-        release_date={featuredMovie?.release_date}
-        backdrop_path={featuredMovie?.backdrop_path}
-        overview={featuredMovie?.overview}
-        vote_average={featuredMovie?.vote_average}
+        contentGenresList={randomMovie?.genre_ids}
+        contentID={randomMovie?.id}
+        title={randomMovie?.title}
+        subTitle={randomMovie?.original_title}
+        release_date={randomMovie?.release_date}
+        backdrop_path={randomMovie?.backdrop_path}
+        overview={randomMovie?.overview}
+        vote_average={randomMovie?.vote_average}
         type={MEDIA_TYPE}
       />
 
