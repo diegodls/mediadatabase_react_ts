2e2c05fa940e5e8853f0bea4a1b3ba8c7c5dfd85
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-03-22 11:30:23 -0300

    refac: move function & add conditional prefix

--- src/components/FeaturedContentDescription.tsx
+++ src/components/FeaturedContentDescription.tsx
@@ -1,5 +1,6 @@
 import { Star } from "phosphor-react";
 import { NavLink } from "react-router-dom";
+import { formatDateFromString } from "../utils/formatDateFromString";
 import { FeaturedContentProps } from "./FeaturedContent";
 import { Section } from "./Section";
 
@@ -17,37 +18,16 @@ export function FeaturedContentDescription({
   showReadMore,
   showInfo = true,
 }: FeaturedContentProps) {
-  const mediaType: string = type === "movie" ? "Filme" : "Seriado";
-  const months = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Maio",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro",
-  ];
-
-  function formatDate(date: string): string {
-    let day = date.split("-")[2];
-    let month = date.split("-")[1];
-    let year = date.split("-")[0];
-    let monthInText = months[Number(month) - 1];
-    return `${day} de ${monthInText} de ${year}`;
-  }
-
+  const mediaTypeInPtBr: string = type === "movie" ? "Filme" : "Seriado";
+  const prefix = type === "movie" ? "o" : "a";
+  const dateFormatted = formatDateFromString(release_date);
   return (
     <Section>
       <div className='md:w-[40%] md:px-2 md:mt-0 md:ml-16 md:left-0 md:top-1/2 md:transform md:-translate-y-1/2 md:absolute md:z-50 md:overflow-hidden'>
         {title ? (
           <h1
-            aria-label={`${mediaType}: ${title}`}
-            title={`${mediaType}: ${title}`}
+            aria-label={`${mediaTypeInPtBr}: ${title}`}
+            title={`${mediaTypeInPtBr}: ${title}`}
             className='md:text-4xl font-bold wrap-text text-2xl'
           >
             {title}
@@ -56,8 +36,8 @@ export function FeaturedContentDescription({
 
         {subTitle && title != subTitle ? (
           <p
-            aria-label={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
-            title={`Nome original do ${mediaType}: ${title} é ${subTitle}`}
+            aria-label={`Nome original d${prefix} ${mediaTypeInPtBr}: ${title} é ${subTitle}`}
+            title={`Nome original d${prefix} ${mediaTypeInPtBr}: ${title} é ${subTitle}`}
             className='font-bold wrap-text text-md line-clamp-1'
           >
             {subTitle}
@@ -66,7 +46,7 @@ export function FeaturedContentDescription({
 
         {overview && showInfo ? (
           <span
-            aria-label={`Resumo do ${mediaType}: ${title}: ${overview}`}
+            aria-label={`Resumo d${prefix} ${mediaTypeInPtBr}: ${title}: ${overview}`}
             title={overview}
             className='mt-2 md:line-clamp-3 line-clamp-2'
           >
@@ -77,12 +57,8 @@ export function FeaturedContentDescription({
         <div className='w-full mt-2 flex items-center justify-between'>
           {release_date ? (
             <span
-              aria-label={`O ${mediaType} ${title} foi lançado em ${formatDate(
-                release_date
-              )}`}
-              title={`O ${mediaType} ${title} foi lançado em ${formatDate(
-                release_date
-              )}`}
+              aria-label={`O dia de lançamento d${prefix} ${mediaTypeInPtBr} ${title} é ${dateFormatted}`}
+              title={`O dia de lançamento d${prefix} ${mediaTypeInPtBr} ${title} é ${dateFormatted}`}
               className='text-lg font-bold line-clamp-3'
             >
               {release_date.split("-")[0]}
@@ -91,8 +67,8 @@ export function FeaturedContentDescription({
 
           {runtime ? (
             <p
-              aria-label={`O ${mediaType} ${title} tem a duração de ${runtime} minutos`}
-              title={`O ${mediaType} ${title} tem a duração de ${runtime} minutos`}
+              aria-label={`${prefix.toUpperCase()} ${mediaTypeInPtBr} ${title} tem a duração de ${runtime} minutos`}
+              title={`${prefix.toUpperCase()} ${mediaTypeInPtBr} ${title} tem a duração de ${runtime} minutos`}
               className='text-lg font-bold line-clamp-3'
             >
               {runtime} min
@@ -101,8 +77,8 @@ export function FeaturedContentDescription({
 
           {vote_average ? (
             <span
-              aria-label={`O ${mediaType} ${title} tem uam popularidade de ${vote_average}`}
-              title={`O ${mediaType} ${title} tem uam popularidade de ${vote_average}s`}
+              aria-label={`${prefix.toUpperCase()} ${mediaTypeInPtBr} ${title} tem uam popularidade de ${vote_average}`}
+              title={`${prefix.toUpperCase()} ${mediaTypeInPtBr} ${title} tem uam popularidade de ${vote_average}s`}
               className='flex'
             >
               <Star
@@ -118,7 +94,8 @@ export function FeaturedContentDescription({
 
           {showReadMore && contentID ? (
             <NavLink
-              to={`${type}/${contentID}`}
+              to={`${contentID}`}
+              end
               className='min-w-auto flex items-center justify-center px-2 bg-customColors-red-500 rounded'
             >
               <p className='p-1 m-auto text-white'>Leia Mais</p>
@@ -127,7 +104,8 @@ export function FeaturedContentDescription({
         </div>
         {genresList && genresList.genres.length > 0 ? (
           <ul
-            aria-label={`Lista dos gêneros do ${mediaType}: ${title}`}
+            aria-label={`Lista dos gêneros d${prefix} ${mediaTypeInPtBr}: ${title}`}
+            title={`Lista dos gêneros d${prefix} ${mediaTypeInPtBr}: ${title}`}
             className='mt-3 flex flex-wrap gap-x-2 row '
           >
             {genresList.genres.map((genre) => {
