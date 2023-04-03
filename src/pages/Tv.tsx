4a2b6e4e2e5ca4a4ae036eb21b45b608ca0efa2f
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-04-03 15:44:51 -0300

    refac: testing new hook to get all data once

--- src/pages/Tv.tsx
+++ src/pages/Tv.tsx
@@ -3,15 +3,28 @@ import { FeaturedContent } from "../components/FeaturedContent";
 import { List } from "../components/List";
 import { Loading } from "../components/Loading";
 import { useFetchData } from "../hooks/useFetchData";
+import { useGetContentsByGenresOnDiscovery } from "../hooks/useGetContentsByGenresOnDiscovery";
 import { useGetRandomByDiscovery } from "../hooks/useGetRandomByDiscovery";
 import { IDiscoveryTv, IDiscoveryTvResult } from "../interfaces/IDiscoveryTv";
 import { IGenres } from "../interfaces/IGenres";
 import { MediaTypes } from "../types/sharedTypes/MediaTypes";
+import { tvGenresMock } from "../utils/Genres";
+import { translateTitle } from "../utils/translateTitle";
 
 export function Tv() {
   const MEDIA_TYPE: MediaTypes = "tv";
 
-  const URL_DISCOVERY_BY_GENRES = `discover/${MEDIA_TYPE}?sort_by=popularity.desc&include_adult=false&page=1&with_genres=`;
+  const GENRES_TO_SHOW: string[] = [
+    "Action & Adventure",
+    "Comédia",
+    "Animação",
+    "Kids",
+    "Família",
+    "Drama",
+    "Mistério",
+    "Documentário",
+    "Crime",
+  ];
 
   const { data: tvGenresList } = useFetchData<IGenres>(
     `genre/${MEDIA_TYPE}/list`
@@ -20,17 +33,18 @@ export function Tv() {
   const { randomContent, loadingRandomContent, randomContentError } =
     useGetRandomByDiscovery<IDiscoveryTvResult>("tv");
 
-  const {
-    data: actionTv,
-    loadingData: loadingActionTv,
-    dataError: errorActionTv,
-  } = useFetchData<IDiscoveryTv>(`${URL_DISCOVERY_BY_GENRES}10759`);
+  const { content, loadingContent, contentError } =
+    useGetContentsByGenresOnDiscovery<IDiscoveryTv>(
+      MEDIA_TYPE,
+      GENRES_TO_SHOW,
+      tvGenresList ? tvGenresList : tvGenresMock
+    );
 
   useEffect(() => {
     document.title = `MDB - Series`;
   }, []);
 
-  const isFetchingData: boolean = loadingRandomContent || loadingActionTv;
+  const isFetchingData: boolean = loadingRandomContent || loadingContent;
 
   return isFetchingData ? (
     <Loading />
@@ -49,13 +63,18 @@ export function Tv() {
         type={MEDIA_TYPE}
         showReadMore={true}
       />
-
-      <List<IDiscoveryTvResult>
-        data={actionTv?.results}
-        error={errorActionTv}
-        title='Ação'
-        type={MEDIA_TYPE}
-      />
+      {content && content.length > 0
+        ? content.map((item) => (
+            <div className='w-full' key={item.genre.id}>
+              <List<IDiscoveryTvResult>
+                data={item.content.results}
+                error={contentError}
+                title={translateTitle(item.genre.name)}
+                type={MEDIA_TYPE}
+              />
+            </div>
+          ))
+        : null}
     </div>
   );
 }
