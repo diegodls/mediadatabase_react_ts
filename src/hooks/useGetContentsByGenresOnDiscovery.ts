8ae31bcd93e11753a5667df26336df41ae863614
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-04-03 15:43:47 -0300

    refac: finishing base code

--- src/hooks/useGetContentsByGenresOnDiscovery.ts
+++ src/hooks/useGetContentsByGenresOnDiscovery.ts
@@ -1,65 +1,118 @@
-import { useState } from "react";
-import { IDiscoveryMoviesResult } from "../interfaces/IDiscoveryMovies";
-import { IDiscoveryTvResult } from "../interfaces/IDiscoveryTv";
+import { useEffect, useState } from "react";
+import {
+  IDiscoveryMovies,
+  IDiscoveryMoviesResult,
+} from "../interfaces/IDiscoveryMovies";
+import { IDiscoveryTv, IDiscoveryTvResult } from "../interfaces/IDiscoveryTv";
 import { IErrorFetchContent } from "../interfaces/IErrorFetchContent";
 import { IGenre, IGenres } from "../interfaces/IGenres";
 import { service } from "../services/api";
-import { MediaTypes } from "../types/sharedTypes/MediaTypes";
+import { MediaTypes } from "./../types/sharedTypes/MediaTypes";
 
-interface IUseGetContentsByGenresOnDiscovery {
+type TDiscovery = IDiscoveryMovies | IDiscoveryTv;
+
+type TDiscoveryResult = IDiscoveryMoviesResult | IDiscoveryTvResult;
+interface IContentState<T> {
+  genre: IGenre;
   type: MediaTypes;
-  genresToShow: string[];
-  genresListFromApi: IGenres;
+  content: T;
+}
+interface IUseGetContentsByGenresOnDiscoveryReturn<T> {
+  content: IContentState<T>[] | undefined;
+  loadingContent: boolean;
+  contentError?: IErrorFetchContent;
+  getContentFromApi: () => void;
 }
 
-type TRandomContentState = IDiscoveryMoviesResult | IDiscoveryTvResult;
-
-async function fetchData<T>(url: string): Promise<T> {
+async function fetchData<T>(url: string): Promise<T | undefined> {
   return await service.get<T>(url).then((response) => {
     return response.data;
   });
 }
 
-async function getGenresID({
-  type,
-  genresToShow,
-  genresListFromApi,
-}: IUseGetContentsByGenresOnDiscovery) {
-  let genresID: number[] = [];
-
-  for (const genre in genresToShow) {
-  }
-}
-
-function filterGenresId(
+function filterGenresID(
   genresToShow: string[],
   genresListFromApi: IGenres
-): IGenre["id"][] {
-  let genresIdList: IGenre["id"][] = [];
+): IGenre[] {
+  let genresIDList: IGenre[] = [];
 
-  for (let i in genresListFromApi.genres) {
+  for (const i in genresListFromApi.genres) {
     if (genresToShow.includes(genresListFromApi.genres[i].name)) {
-      genresIdList.push(genresListFromApi.genres[i].id);
+      genresIDList.push(genresListFromApi.genres[i]);
+      console.log(`${">".repeat(50)}=> ${genresListFromApi.genres[i].name}`);
     }
   }
 
-  return genresIdList;
+  console.log(`${".".repeat(50)}=> genresToShow`);
+  console.log(genresToShow);
+  console.log(`${".".repeat(50)}=> genresListFromApi`);
+  console.log(genresListFromApi);
+  console.log(`${".".repeat(50)}=> genresIDList`);
+  console.log(genresIDList);
+
+  return genresIDList;
 }
 
-export function useGetContentsByGenresOnDiscovery<T>({
-  type,
-  genresToShow,
-  genresListFromApi,
-}: IUseGetContentsByGenresOnDiscovery) {
+async function getContentListByGenresID<T>(
+  type: MediaTypes,
+  genresIDList: IGenre[]
+): Promise<IContentState<T>[]> {
   const URL_DISCOVERY_BY_GENRES = `discover/${type}?sort_by=popularity.desc&include_adult=false&page=1&with_genres=`;
+  let contentArray: IContentState<T>[] = [];
 
-  const [randomContent, setRandomContent] = useState<T>();
-  const [loadingRandomContent, setLoadingRandomContent] =
-    useState<boolean>(true);
-  const [randomContentError, setRandomContentError] =
-    useState<IErrorFetchContent>();
+  for (const genre of genresIDList) {
+    console.log(`${"!".repeat(50)}=> Buscando pelo genero: ${genre.name}`);
 
-  const genresIdList = filterGenresId(genresToShow, genresListFromApi);
+    const data = await fetchData<T>(`${URL_DISCOVERY_BY_GENRES}${genre.id}`);
+    if (data) {
+      contentArray.push({
+        genre: genre,
+        type,
+        content: data,
+      });
+    }
+  }
 
-  return { randomContent, loadingRandomContent, randomContentError };
+  return contentArray;
+}
+
+export function useGetContentsByGenresOnDiscovery<T>(
+  type: MediaTypes,
+  genresToShow: string[],
+  genresListFromApi: IGenres
+): IUseGetContentsByGenresOnDiscoveryReturn<T> {
+  const [content, setContent] = useState<IContentState<T>[]>();
+  const [loadingContent, setLoadingContent] = useState<boolean>(true);
+  const [contentError, setContentError] = useState<IErrorFetchContent>();
+
+  async function getContentFromApi() {
+    setLoadingContent(true);
+    setContentError(undefined);
+    const genresIDList = filterGenresID(genresToShow, genresListFromApi);
+
+    const ContentListFromApi: IContentState<T>[] =
+      await getContentListByGenresID<T>(type, genresIDList);
+
+    if (ContentListFromApi) {
+      setContent(ContentListFromApi);
+      console.log(`${"@".repeat(50)}=>ContentListFromApi`);
+      console.log(ContentListFromApi);
+    } else {
+      setContentError({
+        status_code: 404,
+        success: false,
+        status_message: `Não foi possível localizar a lista de ${
+          type === "movie" ? "filmes" : "series"
+        } utilizando a lista ${genresToShow}`,
+      });
+    }
+
+    setLoadingContent(false);
+  }
+
+  useEffect(() => {
+    getContentFromApi();
+  }, []);
+
+  return { content, loadingContent, contentError, getContentFromApi };
 }
