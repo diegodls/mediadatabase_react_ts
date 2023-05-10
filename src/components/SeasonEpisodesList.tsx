28f245a74f11239fd394b6031212e3c2670d87d6
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-05-10 13:59:36 -0300

    refac: add loading

--- src/components/SeasonEpisodesList.tsx
+++ src/components/SeasonEpisodesList.tsx
@@ -1,20 +1,35 @@
 import { ITvSeasonDetailed } from "../interfaces/ITvSeasonDetailed";
+import { Loading } from "./Loading";
 import { SeasonEpisodeListItem } from "./SeasonEpisodesListItem";
 
 interface SeasonEpisodeListProps {
   currentSeason?: ITvSeasonDetailed;
+  loadingCurrentSeason: boolean;
 }
 
-export function SeasonEpisodeList({ currentSeason }: SeasonEpisodeListProps) {
+export function SeasonEpisodeList({
+  currentSeason,
+  loadingCurrentSeason,
+}: SeasonEpisodeListProps) {
   return (
-    <div className='max-h-[500px] mt-2 overflow-auto scrollbar-thumb-zinc-500 hover:scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin'>
-      <ul className='flex flex-col gap-2 '>
-        {currentSeason && currentSeason.episodes.length > 0
-          ? currentSeason.episodes.map((episode) => (
-              <SeasonEpisodeListItem key={episode.id} episode={episode} />
-            ))
-          : "Sem informações da temporada"}
-      </ul>
+    <div
+      className={`mt-2 h-[400px] rounded  ${
+        loadingCurrentSeason
+          ? "overflow-hidden"
+          : "overflow-auto scrollbar- scrollbar-thin scrollbar-track-transparent scrollbar-thumb-zinc-500 hover:scrollbar-thumb-zinc-700 scrollbar-thumb-rounded-full scrollbar-track-rounded-full"
+      }`}
+    >
+      {loadingCurrentSeason ? (
+        <Loading onTop={false} h_full={true} />
+      ) : (
+        <ul className='flex flex-col gap-2 rounded overflow-hidden'>
+          {currentSeason && currentSeason.episodes.length > 0
+            ? currentSeason.episodes.map((episode) => (
+                <SeasonEpisodeListItem key={episode.id} episode={episode} />
+              ))
+            : "Sem informações da temporada"}
+        </ul>
+      )}
     </div>
   );
 }
