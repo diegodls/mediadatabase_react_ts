4b1cf94df9231297d59a6e3bf6c2e5840b829b72
DiegoL <contato.diegoluisdasilva@gmail.com>
2023-05-10 14:00:01 -0300

    refac: customize item

--- src/components/SeasonEpisodesListItem.tsx
+++ src/components/SeasonEpisodesListItem.tsx
@@ -2,6 +2,7 @@ import { Episode } from "../interfaces/ITvSeasonDetailed";
 
 import backdrop_path_error from "../assets/img/image-error.png";
 
+import { Star } from "phosphor-react";
 import { API_BASEURL_IMAGE_200 } from "../utils/constants";
 
 interface SeasonEpisodeListPropsItem {
@@ -10,8 +11,8 @@ interface SeasonEpisodeListPropsItem {
 
 export function SeasonEpisodeListItem({ episode }: SeasonEpisodeListPropsItem) {
   return (
-    <li className='h-20 flex gap-2 overflow-hidden'>
-      <div className='h-full relative'>
+    <li className='h-20 mr-3 flex rounded border border-customColors-red-500 overflow-hidden'>
+      <div className='min-w-fit h-full overflow-hidden relative'>
         <img
           className='h-full absolute z-20'
           src={API_BASEURL_IMAGE_200 + episode?.still_path}
@@ -27,8 +28,18 @@ export function SeasonEpisodeListItem({ episode }: SeasonEpisodeListPropsItem) {
           loading='lazy'
         />
       </div>
-      <div>
-        <p>{episode?.name}</p>
+      <div className='w-full h-full flex flex-row items-center j'>
+        <div className='w-full h-full pl-2'>
+          <p className='font-bold'>
+            {episode?.episode_number.toString().padStart(2, "0")} -{" "}
+            {episode?.name}
+          </p>
+          <p className='whitespace-wrap line-clamp-2'>{episode?.overview}</p>
+        </div>
+        <div className='h-full p-2 flex flex-col items-center justify-center bg-customColors-red-500'>
+          <Star color='#FFF' weight='fill' />
+          <p>{episode?.vote_average.toFixed(1)}</p>
+        </div>
       </div>
     </li>
   );
